a95ce3d69f9397427df283de43e7d02ebf388854
Jan Engelhardt <jengelh@inai.de>
2014-11-03 10:52:56 +0100

    all: add trk: prefixes to possibly evil connections
    Prefix URLs to Google services with trk: so that whenever something
    tries to load them, the developer will be informed via printf and
    dialog about this infraction.
    
    If you see such dialog, we know that (a) either the URL needs to be
    whitelisted, or (b) the feature that triggered it needs to be disabled
    by default.

--- chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
@@ -1663,7 +1663,7 @@ void FileManagerPrivateInternalGetDownloadUrlFunction::OnGotDownloadUrl(
       SigninManagerFactory::GetForProfile(GetProfile());
   const std::string& account_id = signin_manager->GetAuthenticatedAccountId();
   std::vector<std::string> scopes;
-  scopes.emplace_back("https://www.googleapis.com/auth/drive.readonly");
+  scopes.emplace_back("trk:208:https://www.googleapis.com/auth/drive.readonly");
 
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory =
       content::BrowserContext::GetDefaultStoragePartition(GetProfile())
