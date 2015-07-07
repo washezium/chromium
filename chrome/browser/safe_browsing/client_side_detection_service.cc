e5d80b18f27de7bbd5501aaaa630f5c09d925ade
Joachim Bauch <jojo@struktur.de>
2015-07-07 18:28:46 +0200

    safe_browsing: disable reporting of safebrowsing override
    Disables reporting of the safebrowsing override, i.e. the report sent
    if a user decides to visit a page that was flagged as "insecure".
    This prevents trk:148 (phishing) and trk:149 (malware).

--- chrome/browser/safe_browsing/client_side_detection_service.cc
+++ chrome/browser/safe_browsing/client_side_detection_service.cc
@@ -63,11 +63,13 @@ enum MalwareReportTypes {
   REPORT_RESULT_MAX
 };
 
+#if 0
 void UpdateEnumUMAHistogram(MalwareReportTypes report_type) {
   DCHECK(report_type >= 0 && report_type < REPORT_RESULT_MAX);
   UMA_HISTOGRAM_ENUMERATION("SBClientMalware.SentReports", report_type,
                             REPORT_RESULT_MAX);
 }
+#endif
 
 }  // namespace
 
@@ -287,6 +289,10 @@ void ClientSideDetectionService::StartClientReportPhishingRequest(
     return;
   }
 
+#if 1
+  if (!callback.is_null())
+    callback.Run(GURL(request->url()), false);
+#else
   // Fill in metadata about which model we used.
   if (is_extended_reporting) {
     request->set_model_filename(model_loader_extended_->name());
@@ -369,6 +375,7 @@ void ClientSideDetectionService::StartClientReportPhishingRequest(
 
   // Record that we made a request
   phishing_report_times_.push(base::Time::Now());
+#endif
 }
 
 void ClientSideDetectionService::StartClientReportMalwareRequest(
@@ -383,6 +390,10 @@ void ClientSideDetectionService::StartClientReportMalwareRequest(
     return;
   }
 
+#if 1
+  if (!callback.is_null())
+    callback.Run(GURL(request->url()), GURL(request->url()), false);
+#else
   std::string request_data;
   if (!request->SerializeToString(&request_data)) {
     UpdateEnumUMAHistogram(REPORT_FAILED_SERIALIZATION);
@@ -452,6 +463,7 @@ void ClientSideDetectionService::StartClientReportMalwareRequest(
 
   // Record that we made a malware request
   malware_report_times_.push(base::Time::Now());
+#endif
 }
 
 void ClientSideDetectionService::HandlePhishingVerdict(
