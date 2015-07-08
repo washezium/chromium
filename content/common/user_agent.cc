dc0d71e2b8f9dc78709abd8be0dc75aa4f0aebb2
Jan Engelhardt <jengelh@inai.de>
2015-07-08 15:08:32 +0200

    common: set Iridium version and change User-Agent accordingly
    Reduced version number:
    We do not use @BUILD@ or @PATCH@, in particular not in the User-Agent
    string.
    
    As for the user agent, continue providing Chrome/* for possible
    compatibility checks by the browser and/or websites, since we
    really are still Chromium.
    
    Note to self:
    Update "Chromium/*" in UA string when updating chrome/version.

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -191,6 +191,8 @@ std::string BuildUserAgentFromOSAndProduct(const std::string& os_info,
       product.c_str(),
       WEBKIT_VERSION_MAJOR,
       WEBKIT_VERSION_MINOR);
+  if (strncmp(product.c_str(), "Chrome/", 7) != 0)
+	base::StringAppendF(&user_agent, " Chrome/71.0.0.0");
   return user_agent;
 }
 
