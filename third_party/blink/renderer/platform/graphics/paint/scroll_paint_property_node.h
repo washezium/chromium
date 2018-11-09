SeppPenner/iridium-browser

build: gcc-8: disambiguate SnapContainerData type spec
../../third_party/blink/renderer/platform/graphics/paint/scroll_paint_property_node.h:104:3: error: declaration of ‘base::Optional<cc::SnapContainerData> blink::ScrollPaintPropertyNode::SnapContainerData() const’ [-fpermissive]
   }
   ^
In file included from ../../third_party/blink/renderer/platform/graphics/paint/scroll_paint_property_node.h:17,
                 from ../../third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h:13,
                 from ../../third_party/blink/renderer/platform/graphics/paint/clip_paint_property_node.h:13,
                 from ../../third_party/blink/renderer/core/paint/object_paint_properties.h:15,
                 from ../../third_party/blink/renderer/core/paint/fragment_data.h:9,
                 from ../../third_party/blink/renderer/core/layout/layout_object.h:46,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_model_object.h:34,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_container.h:26,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_hidden_container.h:23,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_resource_container.h:23,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.h:23,
                 from ../../third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.cc:23:
../../third_party/blink/renderer/platform/scroll/scroll_snap_data.h:22:48: error: changes meaning of ‘SnapContainerData’ from ‘using SnapContainerData = class cc::SnapContainerData’ [-fpermissive]
 using SnapContainerData = cc::SnapContainerData;

third_party/blink/renderer/platform/graphics/paint/scroll_paint_property_node.h
@@ -99,7 +99,7 @@ class PLATFORM_EXPORT ScrollPaintPropertyNode
     return state_.overscroll_behavior.y;
   }
 
-  base::Optional<SnapContainerData> SnapContainerData() const {
+  base::Optional<cc::SnapContainerData> SnapContainerData() const {
     return state_.snap_container_data;
   }
 

washezium/chromium

third_party/blink/renderer/platform/graphics/paint/scroll_paint_property_node.h
@@ -105,7 +105,7 @@ class PLATFORM_EXPORT ScrollPaintPropertyNode
     return state_.overscroll_behavior.y;
   }
 
-  base::Optional<SnapContainerData> GetSnapContainerData() const {
+  base::Optional<cc::SnapContainerData> GetSnapContainerData() const {
     return state_.snap_container_data;
   }
 