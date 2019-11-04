souldanger/iridium-browser

prefs: use separate directory for config
Do not overwrite an existing Chromium configuration.

chrome/common/chrome_paths_linux.cc
@@ -64,11 +64,7 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
   base::FilePath config_dir(GetXDGDirectory(env.get(),
                                             kXdgConfigHomeEnvVar,
                                             kDotConfigDir));
-#if defined(GOOGLE_CHROME_BUILD)
-  *result = config_dir.Append("google-chrome");
-#else
-  *result = config_dir.Append("chromium");
-#endif
+  *result = config_dir.Append("iridium");
   return true;
 }
 

washezium/chromium

chrome/common/chrome_paths_linux.cc
@@ -88,11 +88,8 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
         GetXDGDirectory(env.get(), kXdgConfigHomeEnvVar, kDotConfigDir);
   }
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  *result = config_dir.Append("google-chrome" + GetChannelSuffixForDataDir());
-#else
-  *result = config_dir.Append("chromium");
-#endif
+  *result = config_dir.Append("washezium");
+
   return true;
 }
 