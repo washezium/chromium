1cebb765104b440f4357b4028c0c0bf94b9e039e
David Roger <droger@chromium.org>
2020-06-26 09:03:54 +0000

    Revert "[WebAppProvider] Fix test flakiness for AppSorting."
    This reverts commit ce6e5f49aac73a873e0e77053246534a4d649461.
    
    Reason for revert: the test is now flaky. See details in https://crbug.com/1099615
    
    Original change's description:
    > [WebAppProvider] Fix test flakiness for AppSorting.
    > 
    > The test waits for both installations, but this can be flaky as waiting
    > for the first installation could hide the second installation. So when
    > it waits for the second installation, it already happened.
    > 
    > This fix only waits for the second installation to prevent this race
    > from happening.
    > 
    > R=​loyso@chromium.org
    > 
    > Change-Id: I57549f0d1be11e980ecebc6ba7fb8d2ec4e0b7be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268180
    > Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    > Reviewed-by: Chase Phillips <cmp@chromium.org>
    > Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Chase Phillips <cmp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#782775}
    
    TBR=cmp@chromium.org,dmurph@chromium.org,loyso@chromium.org
    
    Change-Id: I69984efe4b14b967eeccc4a59f617725bd648339
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1099615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270178
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782895}

--- chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
@@ -436,7 +436,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest, AppSortingFixCollisions) {
       GetProfile(0), WebappInstallSource::OMNIBOX_INSTALL_ICON,
       GetUserInitiatedAppURL2());
 
-  // Wait for the second install (as they install in order).
+  // Wait for both installs.
+  EXPECT_EQ(WebAppInstallObserver(GetProfile(1), app_id1).AwaitNextInstall(),
+            app_id1);
   EXPECT_EQ(WebAppInstallObserver(GetProfile(1), app_id2).AwaitNextInstall(),
             app_id2);
   EXPECT_TRUE(AllProfilesHaveSameWebAppIds());
