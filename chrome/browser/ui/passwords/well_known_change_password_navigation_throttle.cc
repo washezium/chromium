e01a7fb845bb46db5f1955777d6fd1d1d04e62af
Jonathan Mengedoht <mengedoht@google.com>
2020-06-29 10:06:18 +0000

    Add request to WellKnownChangePasswordNavigationThrottle
    Add request to WellKnownChangePasswordNavigationThrottle for path that is defined to return 404
    
    Bug: 927473
    Change-Id: Iaea5b07887ed6a2b7a2d23878a1dcad660b73b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228840
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783467}

--- chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.cc
+++ chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.cc
@@ -5,9 +5,15 @@
 #include "chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.h"
 
 #include "base/logging.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/storage_partition.h"
+#include "content/public/browser/web_contents.h"
+#include "net/base/load_flags.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
 #include "url/gurl.h"
 
 namespace {
@@ -15,10 +21,23 @@ namespace {
 using content::NavigationHandle;
 using content::NavigationThrottle;
 
+// This path should return 404. This enables us to check whether
+// we can trust the server's response code.
+// https://wicg.github.io/change-password-url/response-code-reliability.html#iana
+constexpr char kNotExistingResourcePath[] =
+    ".well-known/"
+    "resource-that-should-not-exist-whose-status-code-should-not-be-200";
+
 bool IsWellKnownChangePasswordUrl(const GURL& url) {
   return url.is_valid() && url.has_path() &&
          (url.PathForRequest() == "/.well-known/change-password" ||
           url.PathForRequest() == "/.well-known/change-password/");
+}
+
+GURL CreateNonExistingResourceURL(const GURL& url) {
+  GURL::Replacements replacement;
+  replacement.SetPathStr(kNotExistingResourcePath);
+  return url.GetOrigin().ReplaceComponents(replacement);
 }
 
 }  // namespace
@@ -50,6 +69,7 @@ WellKnownChangePasswordNavigationThrottle::
 
 NavigationThrottle::ThrottleCheckResult
 WellKnownChangePasswordNavigationThrottle::WillStartRequest() {
+  FetchNonExistingResource(navigation_handle());
   return NavigationThrottle::PROCEED;
 }
 
@@ -60,9 +80,70 @@ WellKnownChangePasswordNavigationThrottle::WillFailRequest() {
 
 NavigationThrottle::ThrottleCheckResult
 WellKnownChangePasswordNavigationThrottle::WillProcessResponse() {
+  change_password_response_code_ =
+      navigation_handle()->GetResponseHeaders()->response_code();
+  // TODO(crbug.com/927473) handle processing
   return NavigationThrottle::PROCEED;
 }
 
 const char* WellKnownChangePasswordNavigationThrottle::GetNameForLogging() {
   return "WellKnownChangePasswordNavigationThrottle";
+}
+
+void WellKnownChangePasswordNavigationThrottle::FetchNonExistingResource(
+    NavigationHandle* handle) {
+  auto resource_request = std::make_unique<network::ResourceRequest>();
+  resource_request->url = CreateNonExistingResourceURL(handle->GetURL());
+  resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+  resource_request->load_flags = net::LOAD_DISABLE_CACHE;
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation(
+          "well_known_path_that_should_not_exist",
+          R"(
+        semantics {
+          sender: "Password Manager"
+          description:
+            "Check whether the site supports .well-known 'special' URLs."
+            "If the website does not support the spec we navigate to the "
+            "fallback url. See also "
+"https://wicg.github.io/change-password-url/response-code-reliability.html#iana"
+          trigger:
+            "When the user clicks 'Change password' on "
+            "chrome://settings/passwords, or when they visit the "
+            "[ORIGIN]/.well-known/change-password special URL, Chrome makes "
+            "this additional request. Chrome Password manager shows a button "
+            "with the link in the password checkup for compromised passwords "
+            "view (chrome://settings/passwords/check) and in a dialog when the "
+            "user signs in using compromised credentials."
+          data:
+            "The request body is empty. No user data is included."
+          destination: WEBSITE
+        }
+        policy {
+          cookies_allowed: NO
+          setting: "This feature cannot be disabled."
+          policy_exception_justification: "Essential for navigation."
+        })");
+  auto url_loader_factory =
+      content::BrowserContext::GetDefaultStoragePartition(
+          Profile::FromBrowserContext(
+              handle->GetWebContents()->GetBrowserContext()))
+          ->GetURLLoaderFactoryForBrowserProcess();
+  url_loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
+                                                 traffic_annotation);
+  // Binding the callback to |this| is safe, because the navigationthrottle
+  // defers if the request is not received yet. Thereby the throttle still exist
+  // when the response arrives.
+  url_loader_->DownloadHeadersOnly(
+      url_loader_factory.get(),
+      base::BindOnce(&WellKnownChangePasswordNavigationThrottle::
+                         FetchNonExistingResourceCallback,
+                     base::Unretained(this)));
+}
+
+void WellKnownChangePasswordNavigationThrottle::
+    FetchNonExistingResourceCallback(
+        scoped_refptr<net::HttpResponseHeaders> headers) {
+  non_existing_resource_response_code_ = headers->response_code();
+  // TODO(crbug.com/927473) handle processing
 }
--- chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.h
+++ chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.h
@@ -13,6 +13,14 @@ namespace content {
 class NavigationHandle;
 }  // namespace content
 
+namespace net {
+class HttpResponseHeaders;
+}  // namespace net
+
+namespace network {
+class SimpleURLLoader;
+}  // namespace network
+
 // This NavigationThrottle checks whether a site supports the
 // .well-known/change-password url. To check whether a site supports the
 // change-password url, we also request a .well-known path that is defined to
@@ -39,6 +47,15 @@ class WellKnownChangePasswordNavigationThrottle
  private:
   explicit WellKnownChangePasswordNavigationThrottle(
       content::NavigationHandle* handle);
+  // Request the status code from a path that is expected to return 404.
+  void FetchNonExistingResource(content::NavigationHandle* handle);
+  // Callback for the request to the "not exist" path.
+  void FetchNonExistingResourceCallback(
+      scoped_refptr<net::HttpResponseHeaders> headers);
+
+  int non_existing_resource_response_code_ = 0;
+  int change_password_response_code_ = 0;
+  std::unique_ptr<network::SimpleURLLoader> url_loader_;
 };
 
 #endif  // CHROME_BROWSER_UI_PASSWORDS_WELL_KNOWN_CHANGE_PASSWORD_NAVIGATION_THROTTLE_H_
