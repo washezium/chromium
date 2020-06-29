29a4898a424f49b695b9c674971aec88c4a5fe36
David Van Cleve <davidvc@chromium.org>
2020-06-29 21:18:24 +0000

    Trust Tokens: Fix a broken browser test covering the origin trial
    It turns out the command line "create an origin trial token" tool
    defaults to 42-day expiry. It's been 42 days since landing this test
    and, surprise, it's broken! This CL fixes the test by replacing the
    origin trial token with one that expires in 2047. Apologies in advance
    to the sheriff 27 years in the future.
    
    TBR=csharrison
    
    Fixed: 1100405
    Change-Id: I0521ad992eae89f62238621f8d0a7d7b376ac855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274238
    Reviewed-by: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783674}

--- content/test/trust_token_origin_trial_browsertest.cc
+++ content/test/trust_token_origin_trial_browsertest.cc
@@ -171,19 +171,21 @@ class TrustTokenOriginTrialBrowsertest
     if (params->url_request.url == kPageWithOriginTrialToken) {
       // Origin Trials key generated with:
       //
-      // tools/origin_trials/generate_token.py 3 http://localhost:5555 \
-      // TrustTokens
+      // tools/origin_trials/generate_token.py --expire-days 5000 --version 3 \
+      // http://localhost:5555 TrustTokens
+      //
+      // Note that you can't have an origin trial token with expiry more than
+      // 2^31-1 seconds past the epoch, so (for instance) --expire-days 10000
+      // would not have generated a valid token.
       URLLoaderInterceptor::WriteResponse(
           base::ReplaceStringPlaceholders(
               "HTTP/1.1 200 OK\n"
               "Content-type: text/html\n"
               "Origin-Trial: $1\n\n",
-              {"AwXANHkr9VyztB9HgkK9DQmNmka5KTQzYTtjmK0M40eSs3BmMw3DE3Dfq+"
-               "btAHUrqb9KDxSKeJfdkHHjaf8kLgAAAABTeyJvcmlnaW4iOiAiaHR0cDovL2x"
-               "vY"
-               "2FsaG9zdDo1NTU1IiwgImZlYXR1cmUiOiAiVHJ1c3RUb2tlbnMiLCAiZXhwaX"
-               "J5"
-               "IjogMTU5MzQ1MzI1NX0="},
+              {"A220DaFwmOb78vs8TojpryN1mfL9+zHjNDdo+rJTwRcaPkCIzU4/"
+               "vP9pnSHyI2ye8WsoxToBprvd7YH+"
+               "SdR0FgAAAABTeyJvcmlnaW4iOiAiaHR0cDovL2xvY2FsaG9zdDo1NTU1IiwgImZ"
+               "lYXR1cmUiOiAiVHJ1c3RUb2tlbnMiLCAiZXhwaXJ5IjogMjAyNTQ1OTI0MX0="},
               /*offsets=*/nullptr),
           /*body=*/"", params->client.get());
       return true;
@@ -300,10 +302,8 @@ INSTANTIATE_TEST_SUITE_P(ExecutingAllOperations,
 // only when permitted by the origin trials framework (either because
 // configuration specifies that no origin trial token is required, or because an
 // origin trial token is present in the executing context).
-//
-// Failing on all bots, https://crbug.com/1100405
 IN_PROC_BROWSER_TEST_P(TrustTokenOriginTrialBrowsertest,
-                       DISABLED_ProvidesParamsOnlyWhenAllowed) {
+                       ProvidesParamsOnlyWhenAllowed) {
   TestDescription test_description = std::get<1>(GetParam());
 
   URLLoaderInterceptor interceptor(base::BindLambdaForTesting(
