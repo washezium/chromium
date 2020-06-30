133447317672bb18af46156781bd3370c5ec39e5
Bo <boliu@chromium.org>
2020-06-30 19:41:03 +0000

    Revert "android: Catch exception in broken OS implementations"
    This reverts commit 1dd99e49a1efb2773c3903bd38afcf58f0717d50.
    
    Reason for revert:
    It just moves the crash to later
    
    Original change's description:
    > android: Catch exception in broken OS implementations
    > 
    > Some bad OS implementation throw null pointer exception here. Just catch
    > and ignore it.
    > 
    > Bug: 1092978
    > Change-Id: Ia465e6198645c2ff0e5c30b0e3c45eccf6f0508b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238451
    > Commit-Queue: Bo <boliu@chromium.org>
    > Reviewed-by: Theresa  <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776700}
    
    TBR=boliu@chromium.org,twellington@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1092978
    Change-Id: Ica8ae5fbe0e463473b4a3da9ecfca06743d0ae32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276238
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784090}

--- chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbar.java
@@ -34,7 +34,6 @@ import androidx.core.view.accessibility.AccessibilityEventCompat;
 import androidx.core.view.inputmethod.EditorInfoCompat;
 
 import org.chromium.base.ApiCompatibilityUtils;
-import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -726,12 +725,7 @@ public class FindToolbar extends LinearLayout {
         } else {
             mSearchKeyShouldTriggerSearch = false;
         }
-        try {
-            mFindQuery.setText(findText);
-        } catch (NullPointerException e) {
-            // See crbug.com/1092978.
-            Log.w(TAG, "ignoring exception in setText", e);
-        }
+        mFindQuery.setText(findText);
         mSettingFindTextProgrammatically = false;
     }
 
