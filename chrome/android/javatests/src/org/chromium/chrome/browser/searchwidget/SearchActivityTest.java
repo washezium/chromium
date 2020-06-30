12901c32cb6cb39a43a9cd5b7eac9c9645fc9b57
Filip Gorski <fgorski@chromium.org>
2020-06-30 18:08:13 +0000

    Disabling SearchActivity.testImageSearch_* tests on Lollipop
    Failures happen mostly:
    * network_service_out_of_process_chrome_public_test_apk, however
    * chrome_public_test_apk also occasionally fails
    
    TBR=gangwu@chromium.org, tedchoc@chromium.org
    
    Bug: 1100967
    Change-Id: I09180f254ce4566dae3b680b7a5091f474d3528b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274319
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784046}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -15,6 +15,7 @@ import android.app.Instrumentation.ActivityMonitor;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.text.TextUtils;
 import android.view.KeyEvent;
@@ -40,6 +41,7 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -563,6 +565,8 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1100967", sdk_is_less_than = Build.VERSION_CODES.M)
     @Features.
     EnableFeatures({ChromeFeatureList.OMNIBOX_ENABLE_CLIPBOARD_PROVIDER_IMAGE_SUGGESTIONS})
     public void testImageSearch() throws InterruptedException, Exception {
@@ -646,6 +650,8 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1100967", sdk_is_less_than = Build.VERSION_CODES.M)
     @Features.
     EnableFeatures({ChromeFeatureList.OMNIBOX_ENABLE_CLIPBOARD_PROVIDER_IMAGE_SUGGESTIONS})
     public void testImageSearch_OnlyTrustedIntentCanPost() throws InterruptedException, Exception {
