d79d49b87932a859cff1ff3d0be19bd8181771b5
Stephen McGruer <smcgruer@chromium.org>
2020-06-30 18:26:30 +0000

    Roll WPT internal tools to 3016dfa710e2114595431f6506f1c7d5729c2484
    Bug: 1095643
    Change-Id: I7665d6439733773fe655a3241479f7015f43509c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261164
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784056}

--- third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
+++ third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 TARGET_DIR=$DIR/wpt
 REMOTE_REPO="https://github.com/web-platform-tests/wpt.git"
-WPT_HEAD=66af89be218a1c81e89ad786104bf2866d006422
+WPT_HEAD=c427fe5d9414f932217ac4f0dbeb2c6bcacca8d5
 
 function clone {
   # Remove existing repo if already exists.
