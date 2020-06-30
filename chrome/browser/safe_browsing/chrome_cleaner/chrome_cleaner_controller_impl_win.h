16a0ee26e9cf27bf4636b472afda265840a22f96
David Bienvenu <davidbienvenu@google.com>
2020-06-30 18:54:06 +0000

    Revert "Safety check: add tests for the Chrome cleaner child backend"
    This reverts commit 9d1db5b829efe3d7b499e32cd6de7daee889ec0a.
    
    Reason for revert: <breaks chromium waterfall>
    
    Original change's description:
    > Safety check: add tests for the Chrome cleaner child backend
    > 
    > * Test that each possible CCT status results in the corresponding
    >   safety check child status and display string.
    > * Test that CCT being enterprise managed is handled correctly.
    > 
    > Bug: 1087263
    > Change-Id: I84d93408800cb4fbe8d138578c586a0ae89dbfc9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249980
    > Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#783942}
    
    TBR=joenotcharles@chromium.org,aee@chromium.org,rainhard@chromium.org
    
    Change-Id: I581fbbf2447c64613bb08861c160b8ce2f781f3e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1087263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276532
    Reviewed-by: David Bienvenu <davidbienvenu@google.com>
    Commit-Queue: David Bienvenu <davidbienvenu@google.com>
    Cr-Commit-Position: refs/heads/master@{#784070}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.h
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.h
@@ -84,7 +84,6 @@ class ChromeCleanerControllerImpl : public ChromeCleanerController {
   // Force the current controller's state for tests that check the effect of
   // starting and completing reporter runs.
   void SetStateForTesting(State state);
-  void SetIdleForTesting(IdleReason idle_reason);
 
  private:
   ChromeCleanerControllerImpl();
