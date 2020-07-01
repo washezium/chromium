2eec73ab6f297cdb5e38406cb05921bf43c6ac91
Christian Larson <chrila@microsoft.com>
2020-07-01 18:43:55 +0000

    Fix Crash Report - media::VideoCaptureDeviceMFWin::OnError
    Updated MFVideoCallback to have a shutdown method to invalidate the
    pointer to the observer_.  Shutdown is needed because MFVideoCallback is
    a refcounted class and can live longer than the raw pointer to
    observer_.  Added test to validate the crash before the fix and that the
    code update fixes the issue.
    
    Bug: 1099934
    Change-Id: I1a80914f720638a4fda2d2155b547ec12bafd08a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275493
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Christian Larson <chrila@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#784506}

--- media/capture/video/win/video_capture_device_mf_win.cc
+++ media/capture/video/win/video_capture_device_mf_win.cc
@@ -483,11 +483,19 @@ class MFVideoCallback final
   }
 
   IFACEMETHODIMP OnEvent(IMFMediaEvent* media_event) override {
+    base::AutoLock lock(lock_);
+    if (!observer_) {
+      return S_OK;
+    }
     observer_->OnEvent(media_event);
     return S_OK;
   }
 
   IFACEMETHODIMP OnSample(IMFSample* sample) override {
+    base::AutoLock lock(lock_);
+    if (!observer_) {
+      return S_OK;
+    }
     if (!sample) {
       observer_->OnFrameDropped(
           VideoCaptureFrameDropReason::kWinMediaFoundationReceivedSampleIsNull);
@@ -526,10 +534,18 @@ class MFVideoCallback final
     return S_OK;
   }
 
+  void Shutdown() {
+    base::AutoLock lock(lock_);
+    observer_ = nullptr;
+  }
+
  private:
   friend class base::RefCountedThreadSafe<MFVideoCallback>;
   ~MFVideoCallback() {}
-  VideoCaptureDeviceMFWin* observer_;
+
+  // Protects access to |observer_|.
+  base::Lock lock_;
+  VideoCaptureDeviceMFWin* observer_ GUARDED_BY(lock_);
 };
 
 // static
@@ -735,6 +751,9 @@ VideoCaptureDeviceMFWin::~VideoCaptureDeviceMFWin() {
               ? IsHighResolution(selected_video_capability_->supported_format)
               : false);
     }
+  }
+  if (video_callback_) {
+    video_callback_->Shutdown();
   }
 }
 
