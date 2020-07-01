6b8d9984fb8e1e8b8c567bb04b628d734377a24d
Jimmy Gong <jimmyxgong@chromium.org>
2020-07-01 00:25:34 +0000

    Remove unnecessary html files in print management build resources
    - Polymer3 does not require html files in its build resources.
    - Streamlined adding new resource files for print management.
    
    Bug: 1053704
    Change-Id: If9c993aec0380315058296cfc83f90623e5b5871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2259137
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784230}

--- chromeos/components/print_management/print_management_ui.cc
+++ chromeos/components/print_management/print_management_ui.cc
@@ -10,6 +10,7 @@
 #include "chromeos/components/print_management/url_constants.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/grit/chromeos_print_management_resources.h"
+#include "chromeos/grit/chromeos_print_management_resources_map.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -22,6 +23,26 @@ namespace chromeos {
 namespace printing {
 namespace printing_manager {
 namespace {
+
+constexpr char kGeneratedPath[] =
+    "@out_folder@/gen/chromeos/components/print_management/resources/";
+
+void SetUpWebUIDataSource(content::WebUIDataSource* source,
+                          base::span<const GritResourceMap> resources,
+                          const std::string& generated_path,
+                          int default_resource) {
+  for (const auto& resource : resources) {
+    std::string path = resource.name;
+    if (path.rfind(generated_path, 0) == 0) {
+      path = path.substr(generated_path.size());
+    }
+    source->AddResourcePath(path, resource.value);
+  }
+  source->SetDefaultResource(default_resource);
+  source->AddResourcePath("test_loader.html", IDR_WEBUI_HTML_TEST_LOADER);
+  source->AddResourcePath("test_loader.js", IDR_WEBUI_JS_TEST_LOADER);
+}
+
 void AddPrintManagementStrings(content::WebUIDataSource* html_source) {
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
       {"completionStatusCanceled",
@@ -83,43 +104,17 @@ PrintManagementUI::PrintManagementUI(
       network::mojom::CSPDirectiveName::ScriptSrc,
       "script-src chrome://resources chrome://test 'self';");
 
-  html_source->AddResourcePath("print_management.js", IDR_PRINT_MANAGEMENT_JS);
-  html_source->AddResourcePath("test_loader.js", IDR_WEBUI_JS_TEST_LOADER);
-  html_source->AddResourcePath("test_loader.html", IDR_WEBUI_HTML_TEST_LOADER);
+  const auto resources = base::make_span(kChromeosPrintManagementResources,
+                                         kChromeosPrintManagementResourcesSize);
+  SetUpWebUIDataSource(html_source.get(), resources, kGeneratedPath,
+                       IDR_PRINT_MANAGEMENT_INDEX_HTML);
+
   html_source->AddResourcePath("printing_manager.mojom-lite.js",
                                IDR_PRINTING_MANAGER_MOJO_LITE_JS);
-  html_source->AddResourcePath("mojo_interface_provider.js",
-                               IDR_PRINT_MANAGEMENT_MOJO_INTERFACE_PROVIDER_JS);
-  html_source->AddResourcePath("pwa.html", IDR_PRINT_MANAGEMENT_PWA_HTML);
-  html_source->AddResourcePath("manifest.json", IDR_PRINT_MANAGEMENT_MANIFEST);
-  html_source->AddResourcePath("printing_app_icon.svg",
-                               IDR_PRINT_MANAGEMENT_ICON);
-  html_source->AddResourcePath("print_job_entry.html",
-                               IDR_PRINT_MANAGEMENT_PRINT_JOB_ENTRY_HTML);
-  html_source->AddResourcePath("print_job_entry.js",
-                               IDR_PRINT_MANAGEMENT_PRINT_JOB_ENTRY_JS);
-  html_source->AddResourcePath("print_management_fonts_css.html",
-                               IDR_PRINT_MANAGEMENT_FONTS_CSS_HTML);
-  html_source->AddResourcePath("print_management_fonts_css.js",
-                               IDR_PRINT_MANAGEMENT_FONTS_CSS_JS);
-  html_source->AddResourcePath("print_management_shared_css.html",
-                               IDR_PRINT_MANAGEMENT_SHARED_CSS_HTML);
-  html_source->AddResourcePath("print_management_shared_css.js",
-                               IDR_PRINT_MANAGEMENT_SHARED_CSS_JS);
-  html_source->AddResourcePath(
-      "print_job_clear_history_dialog.html",
-      IDR_PRINT_MANAGEMENT_PRINT_JOB_CLEAR_HISTORY_DIALOG_HTML);
-  html_source->AddResourcePath(
-      "print_job_clear_history_dialog.js",
-      IDR_PRINT_MANAGEMENT_PRINT_JOB_CLEAR_HISTORY_DIALOG_JS);
-  html_source->AddResourcePath("icons.html", IDR_PRINT_MANAGEMENT_ICONS_HTML);
-  html_source->AddResourcePath("icons.js", IDR_PRINT_MANAGEMENT_ICONS_JS);
-  html_source->SetDefaultResource(IDR_PRINT_MANAGEMENT_INDEX_HTML);
 
   AddPrintManagementStrings(html_source.get());
 
   if (base::FeatureList::IsEnabled(chromeos::features::kScanningUI)) {
-    html_source->AddResourcePath("scanning.html", IDR_SCANNING_HTML);
     html_source->AddResourcePath("scanning_page.js", IDR_SCANNING_PAGE_JS);
   }
 
