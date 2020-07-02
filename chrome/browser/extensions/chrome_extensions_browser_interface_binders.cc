9836a23125aabb5d6664dd85cc41d6fe6c583e03
Curtis McMullan <curtismcmullan@chromium.org>
2020-07-02 08:08:24 +0000

    input-tools: connect vk to handwriting gestures
    This CL adds the necessary mojo plumbing to connect the vk extension to
    the handwriting service gesture based model.
    
    Bug: 1076172
    Change-Id: I968e2b3b103c9cf84a358874323fc218809a7e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262373
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784730}

--- chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
+++ chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
@@ -39,8 +39,10 @@
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "chromeos/services/ime/public/mojom/input_engine.mojom.h"
+#include "chromeos/services/machine_learning/public/cpp/handwriting_recognizer_manager.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
 #include "chromeos/services/machine_learning/public/mojom/handwriting_recognizer.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/handwriting_recognizer_requestor.mojom.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #endif
@@ -69,6 +71,16 @@ void BindHandwritingRecognizer(
           chromeos::machine_learning::mojom::HandwritingRecognizerSpec::New(
               "en"),
           std::move(receiver), base::DoNothing());
+}
+
+void BindHandwritingRecognizerRequestor(
+    content::RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<
+        chromeos::machine_learning::mojom::HandwritingRecognizerRequestor>
+        receiver) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  chromeos::machine_learning::HandwritingRecognizerManager::GetInstance()
+      ->AddReceiver(std::move(receiver));
 }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
@@ -179,6 +191,9 @@ void PopulateChromeFrameBindersForExtension(
         base::BindRepeating(&BindInputEngineManager));
     binder_map->Add<chromeos::machine_learning::mojom::HandwritingRecognizer>(
         base::BindRepeating(&BindHandwritingRecognizer));
+    binder_map->Add<
+        chromeos::machine_learning::mojom::HandwritingRecognizerRequestor>(
+        base::BindRepeating(&BindHandwritingRecognizerRequestor));
   }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
