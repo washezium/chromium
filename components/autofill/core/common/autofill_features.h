aa3be458f8202e5e720b57be3fa1961735b729f2
Matthias Körber <koerber@google.com>
2020-07-02 17:13:50 +0000

    [Autofill] Allow address profile import from unfocusable fields
    Currently, Autofill does not import address profile data from fields
    that are not focusable. This is problematic because such fields are
    often used in combination with synthetic form fields. To resolve this,
    a finch flag is implemented that allows importing profiles from
    unfocusable fields.
    
    This is a reland of f2db4e690f32338128a0b678b5b3629361b7ab93
    to fix a bad name for a finch experiment.
    
    Bug: 1101280
    Change-Id: I9e8e734942dee897fa63959cc1f0cc441f0419e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278266
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Auto-Submit: Matthias Körber <koerber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#784863}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -48,6 +48,7 @@ extern const base::Feature kAutofillOffNoServerData;
 extern const base::Feature kAutofillOverrideWithRaterConsensus;
 extern const base::Feature kAutofillPreferServerNamePredictions;
 extern const base::Feature kAutofillProfileClientValidation;
+extern const base::Feature kAutofillProfileImportFromUnfocusableFields;
 extern const base::Feature kAutofillProfileImportFromUnifiedSection;
 extern const base::Feature kAutofillProfileServerValidation;
 extern const base::Feature kAutofillRejectCompanyBirthyear;
