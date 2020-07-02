4c03194fe748f56cca1b668f195ca71efc20474b
Kalvin Lee <kdlee@chromium.org>
2020-07-02 00:18:21 +0000

    PpdProvider v3: implement CreateV3Provider()
    This change declares a new free function to instantiate the
    the PpdProvider class. Eventually, this function will replace
    the static Create() method declared by the PpdProvider interface.
    
    Bug: chromium:888189
    Change-Id: Id22304f145eb5b56bfc646a90d87558ff93d7910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274924
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784635}

--- chromeos/printing/ppd_provider_v3.cc
+++ chromeos/printing/ppd_provider_v3.cc
@@ -3,12 +3,17 @@
 // found in the LICENSE file.
 
 #include <string>
+#include <utility>
 
 #include "base/memory/scoped_refptr.h"
+#include "base/notreached.h"
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
-#include "chromeos/printing/ppd_provider.h"
+#include "chromeos/printing/ppd_cache.h"
+#include "chromeos/printing/ppd_metadata_manager.h"
+#include "chromeos/printing/ppd_provider_v3.h"
+#include "chromeos/printing/printer_config_cache.h"
 
 namespace chromeos {
 namespace {
@@ -18,15 +23,19 @@ namespace {
 
 class PpdProviderImpl : public PpdProvider {
  public:
-  PpdProviderImpl(const std::string& browser_locale,
+  PpdProviderImpl(base::StringPiece browser_locale,
                   const base::Version& current_version,
-                  const PpdProvider::Options& options)
-      : browser_locale_(browser_locale),
+                  scoped_refptr<PpdCache> cache,
+                  std::unique_ptr<PpdMetadataManager> metadata_manager,
+                  std::unique_ptr<PrinterConfigCache> config_cache)
+      : browser_locale_(std::string(browser_locale)),
+        version_(current_version),
+        cache_(cache),
+        metadata_manager_(std::move(metadata_manager)),
+        config_cache_(std::move(config_cache)),
         file_task_runner_(base::ThreadPool::CreateSequencedTaskRunner(
             {base::TaskPriority::USER_VISIBLE, base::MayBlock(),
-             base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})),
-        version_(current_version),
-        options_(options) {}
+             base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})) {}
 
   void ResolveManufacturers(ResolveManufacturersCallback cb) override {
     // TODO(crbug.com/888189): implement this.
@@ -65,19 +74,26 @@ class PpdProviderImpl : public PpdProvider {
   // BrowserContext::GetApplicationLocale();
   const std::string browser_locale_;
 
-  // Where to run disk operations.
-  const scoped_refptr<base::SequencedTaskRunner> file_task_runner_;
-
   // Current version used to filter restricted ppds
   const base::Version version_;
 
-  // Construction-time options, immutable.
-  const PpdProvider::Options options_;
+  // Provides PPD storage on-device.
+  scoped_refptr<PpdCache> cache_;
+
+  // Interacts with and controls PPD metadata.
+  std::unique_ptr<PpdMetadataManager> metadata_manager_;
+
+  // Fetches PPDs from the Chrome OS Printing team's serving root.
+  std::unique_ptr<PrinterConfigCache> config_cache_;
+
+  // Where to run disk operations.
+  const scoped_refptr<base::SequencedTaskRunner> file_task_runner_;
+
+  base::WeakPtrFactory<PpdProviderImpl> weak_factory_{this};
 };
 
 }  // namespace
 
-// TODO(crbug.com/888189): inline this into the header file.
 PrinterSearchData::PrinterSearchData() = default;
 PrinterSearchData::PrinterSearchData(const PrinterSearchData& other) = default;
 PrinterSearchData::~PrinterSearchData() = default;
@@ -89,10 +105,20 @@ scoped_refptr<PpdProvider> PpdProvider::Create(
     scoped_refptr<PpdCache> ppd_cache,
     const base::Version& current_version,
     const PpdProvider::Options& options) {
-  // TODO(crbug.com/888189): use |loader_factory| and do away with
-  // |ppd_cache|.
-  return base::MakeRefCounted<PpdProviderImpl>(browser_locale, current_version,
-                                               options);
+  NOTREACHED();  // TODO(crbug.com/888189): deprecate this Create().
+  return nullptr;
+}
+
+// free function; _not_ static
+scoped_refptr<PpdProvider> CreateV3Provider(
+    base::StringPiece browser_locale,
+    const base::Version& current_version,
+    scoped_refptr<PpdCache> cache,
+    std::unique_ptr<PpdMetadataManager> metadata_manager,
+    std::unique_ptr<PrinterConfigCache> config_cache) {
+  return base::MakeRefCounted<PpdProviderImpl>(
+      browser_locale, current_version, cache, std::move(metadata_manager),
+      std::move(config_cache));
 }
 
 }  // namespace chromeos
