efbb4f96fed7d0d7daef18f91c6671a4c2a12d89
Guillaume Jenkins <gujen@google.com>
2020-07-02 16:36:30 +0000

    [iOS CBCM] Move CBCM Registrar to components
    Moves ChromeBrowserCloudManagementRegistrar to components/enterprise so
    it can be reused by iOS code.
    
    Bug: 1091930
    Change-Id: Id91f8f85191cb14a0afde72debb7e172a3e236a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270348
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784847}

--- components/enterprise/browser/controller/chrome_browser_cloud_management_helper.cc
+++ components/enterprise/browser/controller/chrome_browser_cloud_management_helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/policy/cloud/chrome_browser_cloud_management_helper.h"
+#include "components/enterprise/browser/controller/chrome_browser_cloud_management_helper.h"
 
 #include <utility>
 #include <vector>
