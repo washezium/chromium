749b1802fe7057479592d13c8c22dc9f23218859
Nidhi Jaju <nidhijaju@google.com>
2020-07-02 03:10:48 +0000

    Change name of vector in GetRegisteredOriginsCallback
    The vector is initially called |callback| but it makes more sense for
    it to be called |origins| as it is a vector of origins. Hence, this
    name was changed in this CL.
    
    Change-Id: Icf24c1b906c5614e4dab8e744c4d5abf13624b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275291
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@google.com>
    Cr-Commit-Position: refs/heads/master@{#784681}

--- content/browser/service_worker/service_worker_storage.h
+++ content/browser/service_worker/service_worker_storage.h
@@ -71,7 +71,7 @@ class CONTENT_EXPORT ServiceWorkerStorage {
   using ResourceList =
       std::vector<storage::mojom::ServiceWorkerResourceRecordPtr>;
   using GetRegisteredOriginsCallback =
-      base::OnceCallback<void(std::vector<url::Origin> callback)>;
+      base::OnceCallback<void(std::vector<url::Origin> origins)>;
   using FindRegistrationDataCallback = base::OnceCallback<void(
       storage::mojom::ServiceWorkerRegistrationDataPtr data,
       std::unique_ptr<ResourceList> resources,
