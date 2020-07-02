1b4b12d73e8438555005a8f2f45e79f68c0be64a
Sorin Jianu <sorin@chromium.org>
2020-07-02 00:12:46 +0000

    Fix operator=(const UpdateState&).
    Change-Id: I410904f5292f1da696dfda3d4fc5944fe2c8f743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278619
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784628}

--- chrome/updater/unittest_util.cc
+++ chrome/updater/unittest_util.cc
@@ -95,7 +95,7 @@ bool operator==(const UpdateService::UpdateState& lhs,
   return versions_equal && lhs.app_id == rhs.app_id && lhs.state == rhs.state &&
          lhs.downloaded_bytes == rhs.downloaded_bytes &&
          lhs.total_bytes == rhs.total_bytes &&
-         lhs.install_progress == rhs.total_bytes &&
+         lhs.install_progress == rhs.install_progress &&
          lhs.error_category == rhs.error_category &&
          lhs.error_code == rhs.error_code && lhs.extra_code1 == rhs.extra_code1;
 }
