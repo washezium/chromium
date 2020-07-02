1894295b44b9c11a7fb49c3b57bb4d86fdc08402
Jeremy Roman <jbroman@chromium.org>
2020-07-02 16:29:34 +0000

    Disable more of TwoClientWebAppsBMOSync in sanitizer builds.
    Failures spotted on Mac ASAN 64:
      https://ci.chromium.org/p/chromium/builders/ci/Mac%20ASan%2064%20Tests%20%281%29/63655
    
    Bug: 1099847
    Change-Id: I345e1a3ae073028e87b1d332f9d97d8426599dd5
    Tbr: jkrcal@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274542
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784844}

--- chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
@@ -182,8 +182,8 @@ IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
   EXPECT_TRUE(AllProfilesHaveSameWebAppIds());
 }
 
-// Flakily fails on Linux TSAN only (crbug.com/1099847)
-#if defined(OS_LINUX)
+// Flakily fails on Mac ASAN, Linux TSAN: crbug.com/1099847
+#if defined(THREAD_SANITIZER) || defined(ADDRESS_SANITIZER)
 #define MAYBE_SyncDoubleInstallationDifferentNames \
   DISABLED_SyncDoubleInstallationDifferentNames
 #else
@@ -219,8 +219,16 @@ IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
   EXPECT_EQ(GetRegistrar(GetProfile(1)).GetAppShortName(app_id), "Test name 2");
 }
 
+// Flakily fails on Mac ASAN: crbug.com/1099847
+#if defined(THREAD_SANITIZER) || defined(ADDRESS_SANITIZER)
+#define MAYBE_SyncDoubleInstallationDifferentUserDisplayMode \
+  DISABLED_SyncDoubleInstallationDifferentUserDisplayMode
+#else
+#define MAYBE_SyncDoubleInstallationDifferentUserDisplayMode \
+  SyncDoubleInstallationDifferentUserDisplayMode
+#endif
 IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
-                       SyncDoubleInstallationDifferentUserDisplayMode) {
+                       MAYBE_SyncDoubleInstallationDifferentUserDisplayMode) {
   ASSERT_TRUE(SetupSync());
   ASSERT_TRUE(AllProfilesHaveSameWebAppIds());
 
