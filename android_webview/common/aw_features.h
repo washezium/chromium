1ca6379c8c12bd5a76955607b5567e7b831176bf
Takashi Toyoshima <toyoshim@chromium.org>
2020-07-03 04:32:38 +0000

    OOR-CORS: Add kWebViewOriginCheckForStreamReader as a kill-switch
    This patch adds a new WebView specific base::Feature to control
    newly introduced origin checks to reduce unexpected compatibility
    breakages.
    
    (cherry picked from commit e21eb00e49a7b17c007d3ba3fdc5af10cf9b5381)
    
    Bug: 1096677
    Change-Id: I28c9fadc6ee5ce0f06017c4977358a3566d6c159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265709
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#784021}
    TBR: torne@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279257
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4183@{#111}
    Cr-Branched-From: 740e9e8a40505392ba5c8e022a8024b3d018ca65-refs/heads/master@{#782793}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -17,6 +17,7 @@ namespace features {
 extern const base::Feature kWebViewBrotliSupport;
 extern const base::Feature kWebViewConnectionlessSafeBrowsing;
 extern const base::Feature kWebViewExtraHeadersSameOriginOnly;
+extern const base::Feature kWebViewOriginCheckForStreamReader;
 extern const base::Feature kWebViewSniffMimeType;
 extern const base::Feature kWebViewTestFeature;
 extern const base::Feature kWebViewWideColorGamutSupport;
