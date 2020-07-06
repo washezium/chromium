1a37310bb2d9a75ff8e9df259f84881bde7fe955
Eugene Zemtsov <eugene@chromium.org>
2020-07-06 10:53:53 +0000

    Replace a histogram macro with a function
    Apparently macros don't support histograms with suffixes, and we need
    to use a function for roughness, otherwise it triggers a DCHECK when
    the name changes.
    
    Bug: 1101907
    Change-Id: Ie64da90f3a1929653f5c1a6453b680aad4646990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2280465
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785324}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -12,7 +12,7 @@
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task_runner_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -185,7 +185,7 @@ void LogRoughness(media::MediaLog* media_log,
 
   // Only report known FPS buckets, on 60Hz displays and at least HD quality.
   if (suffix != nullptr && refresh_rate_hz == 60 && frame_size.height() > 700) {
-    UMA_HISTOGRAM_CUSTOM_TIMES(
+    base::UmaHistogramCustomTimes(
         base::JoinString({kRoughnessHistogramName, suffix}, "."),
         base::TimeDelta::FromMillisecondsD(roughness),
         base::TimeDelta::FromMilliseconds(1),
