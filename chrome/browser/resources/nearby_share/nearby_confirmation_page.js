27fc7c5bb902eb63ab8ea0f0263d101e91a3317b
Richard Knoll <knollr@chromium.org>
2020-07-06 14:04:34 +0000

    [Nearby] Add structure to confirmation page
    This adds the basic HTML structure to the confirmation page. All data is
    still static placeholder text until the mojo interface is ready.
    
    Bug: 1099710
    Change-Id: I183c14af29cd6c021f1cb2f695f6a62cf44527df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276289
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785361}

--- chrome/browser/resources/nearby_share/nearby_confirmation_page.js
+++ chrome/browser/resources/nearby_share/nearby_confirmation_page.js
@@ -8,6 +8,8 @@
  * are not currently in the contacts of this user.
  */
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 import './nearby_preview.js';
 import './nearby_progress.js';
 
