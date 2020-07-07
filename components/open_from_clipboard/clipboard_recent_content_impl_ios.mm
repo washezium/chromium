f8deefa68608dc756a95b2b9e91ecefd9c1623ba
Robbie Gibson <rkgibson@google.com>
2020-07-07 16:41:57 +0000

    [iOS] Only use clipboard changeCount to detect changes
    The major impetus for this change is because the hashing method to
    detect clipboard changes triggers a notification on iOS 14 saying that
    Chrome has accessed the clipboard.
    
    Of the two deleted radar comments, 21833556 looks to have been fixed and
    7619972 is no longer necessary from a privacy standpoint. Unfortunately,
    7619972 means that crbug.com/1058487 will reappear once this CL is
    submitted.
    
    (cherry picked from commit b657fa2b82e4322b67bc16405b011770a0b3b34d)
    
    Bug: 1098722
    Change-Id: I95bdde5c791be4afcba2e1aa771970de09eef67f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282929
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#785374}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285002
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/branch-heads/4183@{#200}
    Cr-Branched-From: 740e9e8a40505392ba5c8e022a8024b3d018ca65-refs/heads/master@{#782793}

--- components/open_from_clipboard/clipboard_recent_content_impl_ios.mm
+++ components/open_from_clipboard/clipboard_recent_content_impl_ios.mm
@@ -7,7 +7,6 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <UIKit/UIKit.h>
 
-#include "base/hash/md5.h"
 #import "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/system/sys_info.h"
@@ -24,45 +23,6 @@ NSString* const kPasteboardChangeCountKey = @"PasteboardChangeCount";
 // Key used to store the last date at which it was detected that the pasteboard
 // changed. It is used to evaluate the age of the pasteboard's content.
 NSString* const kPasteboardChangeDateKey = @"PasteboardChangeDate";
-// Key used to store the hash of the content of the pasteboard. Whenever the
-// hash changed, the pasteboard content is considered to have changed.
-NSString* const kPasteboardEntryMD5Key = @"PasteboardEntryMD5";
-
-// Compute a hash consisting of the first 4 bytes of the MD5 hash of |string|,
-// |image_data|, and |url|. This value is used to detect pasteboard content
-// change. Keeping only 4 bytes is a privacy requirement to introduce collision
-// and allow deniability of having copied a given string, image, or url.
-//
-// |image_data| is passed in as NSData instead of UIImage because converting
-// UIImage to NSData can be slow for large images and getting NSData directly
-// from the pasteboard is quicker.
-NSData* WeakMD5FromPasteboardData(NSString* string,
-                                  NSData* image_data,
-                                  NSURL* url) {
-  base::MD5Context ctx;
-  base::MD5Init(&ctx);
-
-  const std::string clipboard_string = base::SysNSStringToUTF8(string);
-  base::MD5Update(&ctx, clipboard_string);
-
-  // This hash is used only to tell if the image has changed, so
-  // limit the number of bytes to hash to prevent slowdown.
-  NSUInteger bytes_to_hash = fmin([image_data length], 1000000);
-  if (bytes_to_hash > 0) {
-    base::MD5Update(&ctx, base::StringPiece(
-                              reinterpret_cast<const char*>([image_data bytes]),
-                              bytes_to_hash));
-  }
-
-  const std::string url_string = base::SysNSStringToUTF8([url absoluteString]);
-  base::MD5Update(&ctx, url_string);
-
-  base::MD5Digest digest;
-  base::MD5Final(&digest, &ctx);
-
-  NSData* data = [NSData dataWithBytes:digest.a length:4];
-  return data;
-}
 
 }  // namespace
 
@@ -73,8 +33,6 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
 @property(nonatomic, strong) NSUserDefaults* sharedUserDefaults;
 // The pasteboard's change count. Increases everytime the pasteboard changes.
 @property(nonatomic) NSInteger lastPasteboardChangeCount;
-// MD5 hash of the last registered pasteboard entry.
-@property(nonatomic, strong) NSData* lastPasteboardEntryMD5;
 // Contains the authorized schemes for URLs.
 @property(nonatomic, readonly) NSSet* authorizedSchemes;
 // Delegate for metrics.
@@ -82,8 +40,8 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
 // Maximum age of clipboard in seconds.
 @property(nonatomic, readonly) NSTimeInterval maximumAgeOfClipboard;
 
-// If the content of the pasteboard has changed, updates the change count,
-// change date, and md5 of the latest pasteboard entry if necessary.
+// If the content of the pasteboard has changed, updates the change count
+// and change date.
 - (void)updateIfNeeded;
 
 // Returns whether the pasteboard changed since the last time a pasteboard
@@ -108,7 +66,6 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
 
 @synthesize lastPasteboardChangeCount = _lastPasteboardChangeCount;
 @synthesize lastPasteboardChangeDate = _lastPasteboardChangeDate;
-@synthesize lastPasteboardEntryMD5 = _lastPasteboardEntryMD5;
 @synthesize sharedUserDefaults = _sharedUserDefaults;
 @synthesize authorizedSchemes = _authorizedSchemes;
 @synthesize delegate = _delegate;
@@ -149,63 +106,9 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
   [self updateIfNeeded];
 }
 
-- (NSData*)getCurrentMD5 {
-  NSString* pasteboardString = [UIPasteboard generalPasteboard].string;
-  NSData* pasteboardImageData = [[UIPasteboard generalPasteboard]
-      dataForPasteboardType:(NSString*)kUTTypeImage];
-  NSURL* pasteboardURL = [UIPasteboard generalPasteboard].URL;
-  NSData* md5 = WeakMD5FromPasteboardData(pasteboardString, pasteboardImageData,
-                                          pasteboardURL);
-
-  return md5;
-}
-
 - (BOOL)hasPasteboardChanged {
-  // If |MD5Changed|, we know for sure there has been at least one pasteboard
-  // copy since last time it was checked.
-  // If the pasteboard content is still the same but the device was not
-  // rebooted, the change count can be checked to see if it changed.
-  // Note: due to a mismatch between the actual behavior and documentation, and
-  // lack of consistency on different reboot scenarios, the change count cannot
-  // be checked after a reboot.
-  // See radar://21833556 for more information.
-  BOOL deviceRebooted = [self clipboardContentAge] >= [self uptime];
-
-  // On iOS 13, there is a bug where every time a UITextField is opened, the
-  // changeCount increases by 2. Thus, if the difference in counts is even,
-  // it is unknown whether there is a real change or the user just focused some
-  // UITextFields.
-  // See radar://7619972 or crbug.com/1058487 for more information.
-  NSInteger changeCount = [UIPasteboard generalPasteboard].changeCount;
-  BOOL changeCountChanged = changeCount != self.lastPasteboardChangeCount;
-  if (@available(iOS 13, *)) {
-    // No-op. This should be if !available(13), but that is not supported.
-  } else {
-    if (!deviceRebooted) {
-      return changeCountChanged;
-    }
-  }
-
-  // If there was no reboot, and the number hasn't changed, the pasteboard
-  // definitely hasn't changed.
-  if (!deviceRebooted && !changeCountChanged) {
-    return NO;
-  }
-
-  // If there was no reboot and the size of the change is odd , the pasteboard
-  // definitely has changed.
-  BOOL changeCountIncreasedIsOdd =
-      (changeCount - self.lastPasteboardChangeCount) % 2 != 0;
-  if (!deviceRebooted && changeCountIncreasedIsOdd) {
-    return YES;
-  }
-
-  // Otherwise, it is unknown whether or not there was a real change, so
-  // fallback to looking at the MD5.
-  self.lastPasteboardChangeCount = changeCount;
-  BOOL md5Changed =
-      ![[self getCurrentMD5] isEqualToData:self.lastPasteboardEntryMD5];
-  return md5Changed;
+  return UIPasteboard.generalPasteboard.changeCount !=
+         self.lastPasteboardChangeCount;
 }
 
 - (NSURL*)recentURLFromClipboard {
@@ -272,7 +175,6 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
 
   self.lastPasteboardChangeDate = [NSDate date];
   self.lastPasteboardChangeCount = [UIPasteboard generalPasteboard].changeCount;
-  self.lastPasteboardEntryMD5 = [self getCurrentMD5];
 
   [self saveToUserDefaults];
 }
@@ -298,8 +200,6 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
       [self.sharedUserDefaults integerForKey:kPasteboardChangeCountKey];
   self.lastPasteboardChangeDate = base::mac::ObjCCastStrict<NSDate>(
       [self.sharedUserDefaults objectForKey:kPasteboardChangeDateKey]);
-  self.lastPasteboardEntryMD5 = base::mac::ObjCCastStrict<NSData>(
-      [self.sharedUserDefaults objectForKey:kPasteboardEntryMD5Key]);
 }
 
 - (void)saveToUserDefaults {
@@ -307,8 +207,6 @@ NSData* WeakMD5FromPasteboardData(NSString* string,
                                forKey:kPasteboardChangeCountKey];
   [self.sharedUserDefaults setObject:self.lastPasteboardChangeDate
                               forKey:kPasteboardChangeDateKey];
-  [self.sharedUserDefaults setObject:self.lastPasteboardEntryMD5
-                              forKey:kPasteboardEntryMD5Key];
 }
 
 - (NSTimeInterval)uptime {
