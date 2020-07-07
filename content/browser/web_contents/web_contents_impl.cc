f4332f963cd7b01a75f54308b7e8930250233901
Evan Stade <estade@chromium.org>
2020-07-07 20:05:31 +0000

    Speculative fix for app modal dialog crash
    Cancel JS dialogs when a WebContents's delegate is set to null, as the
    dialog can't show without a WebContentsDelegate.
    
    Bug: 1060986
    Change-Id: I5272faa23d5461fc8251887d9bc02f03c54e37e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283388
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785954}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1102,6 +1102,11 @@ void WebContentsImpl::SetDelegate(WebContentsDelegate* delegate) {
     return;
   if (delegate_)
     delegate_->Detach(this);
+
+  CancelActiveAndPendingDialogs();
+  // Since |dialog_manager_| was provided by |delegate_|, reset it.
+  dialog_manager_ = nullptr;
+
   delegate_ = delegate;
   if (delegate_) {
     delegate_->Attach(this);
@@ -1179,9 +1184,8 @@ RenderViewHostImpl* WebContentsImpl::GetRenderViewHost() {
 }
 
 void WebContentsImpl::CancelActiveAndPendingDialogs() {
-  if (dialog_manager_) {
+  if (dialog_manager_)
     dialog_manager_->CancelDialogs(this, /*reset_state=*/false);
-  }
   if (browser_plugin_embedder_)
     browser_plugin_embedder_->CancelGuestDialogs();
 }
