cdcb8e587e36380d4534753b08325165b8364f9c
Tsuyoshi Horo <horo@chromium.org>
2020-07-08 03:25:59 +0000

    Disable testTabNavigationHistoryDoNotLeakBetweenActivities
    testTabNavigationHistoryDoNotLeakBetweenActivities__INCOGNITO_CCT_REGULAR_TAB is flaky
    
    TBR=roagarwal@chromium.org,roagarwal@chromium.org,peconn@chromium.org,bsazonov@chromium.org,rhalavati@chromium.org
    
    Bug: 1102755
    Change-Id: I8deda3ce5feeef1ef07492c099ce7394dbead235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286250
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786144}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoHistoryLeakageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoHistoryLeakageTest.java
@@ -25,6 +25,7 @@ import org.chromium.base.test.params.ParameterProvider;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -166,6 +167,7 @@ public class IncognitoHistoryLeakageTest {
 
     @Test
     @LargeTest
+    @DisabledTest(message = "https://crbug.com/1102755")
     @UseMethodParameter(AllTypesToAllTypes.class)
     public void testTabNavigationHistoryDoNotLeakBetweenActivities(
             String activityType1, String activityType2) throws TimeoutException {
