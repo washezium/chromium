washezium/chromium

commit 7bb748368975670b485d9b2c75301eeb32818b34

Refactor Android MediaSession UI code.
This is the first refactor that prepares the code for
componentization. After further refacrtoring, the next step
will be to relocate the relevant code and resources
to //components/browser_ui/media_session/.

This patch splits MediaSessionTabHelper into two classes,
separated by what is componentizable and what is Chrome
specific.

Most of this class is moved into a new class MediaSessionHelper,
which operates on a WebContents rather than a (Chrome) Tab.
MediaSessionTabHelper remains as a thin wrapper and implementor
of MediaSessionHelper.Delegate. The most substantive change is
moving functionality from TabObserver (in MediaSessionTabHelper)
to WebContentsObserver (in MediaSessionHelper).

Bug: 1066263
Change-Id: I9259a082686c0e0f6b9788bc15a19e887c0e95e6
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204964
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
Cr-Commit-Position: refs/heads/master@{#777374}

chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationFaviconTest.java
@@ -157,7 +157,7 @@ public class MediaNotificationFaviconTest extends MediaNotificationManagerTestBa
     }
 
     private Bitmap getDisplayedIcon() {
-        return mTabHolder.mMediaSessionTabHelper.mFavicon;
+        return mTabHolder.mMediaSessionTabHelper.mMediaSessionHelper.mFavicon;
     }
 
     @Override

chrome/android/chrome_java_sources.gni
@@ -900,9 +900,11 @@ chrome_java_sources = [
   "java/src/org/chromium/chrome/browser/media/remote/RecordCastAction.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaImageCallback.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaImageManager.java",
+  "java/src/org/chromium/chrome/browser/media/ui/MediaNotificationImageUtils.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaNotificationInfo.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaNotificationListener.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaNotificationManager.java",
+  "java/src/org/chromium/chrome/browser/media/ui/MediaSessionHelper.java",
   "java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java",
   "java/src/org/chromium/chrome/browser/metrics/ActivityTabStartupMetricsTracker.java",
   "java/src/org/chromium/chrome/browser/metrics/BackgroundTaskMemoryMetricsEmitter.java",

chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaNotificationImageUtils.java
@@ -1,0 +1,74 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.media.ui;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.base.SysUtils;
+
+/** A collection of utilities and constants for the images used in MediaSession notifications. */
+public class MediaNotificationImageUtils {
+    static final int MINIMAL_MEDIA_IMAGE_SIZE_PX = 114;
+
+    // The media artwork image resolution on high-end devices.
+    private static final int HIGH_IMAGE_SIZE_PX = 512;
+
+    // The media artwork image resolution on high-end devices.
+    private static final int LOW_IMAGE_SIZE_PX = 256;
+
+    /**
+     * Downscale |icon| for display in the notification if needed. Returns null if |icon| is null.
+     * If |icon| is larger than {@link getIdealMediaImageSize()}, scale it down to
+     * {@link getIdealMediaImageSize()} and return. Otherwise return the original |icon|.
+     * @param icon The icon to be scaled.
+     */
+    @Nullable
+    public static Bitmap downscaleIconToIdealSize(@Nullable Bitmap icon) {
+        if (icon == null) return null;
+
+        int targetSize = getIdealMediaImageSize();
+
+        Matrix m = new Matrix();
+        int dominantLength = Math.max(icon.getWidth(), icon.getHeight());
+
+        if (dominantLength < getIdealMediaImageSize()) return icon;
+
+        // Move the center to (0,0).
+        m.postTranslate(icon.getWidth() / -2.0f, icon.getHeight() / -2.0f);
+        // Scale to desired size.
+        float scale = 1.0f * targetSize / dominantLength;
+        m.postScale(scale, scale);
+        // Move to the desired place.
+        m.postTranslate(targetSize / 2.0f, targetSize / 2.0f);
+
+        // Draw the image.
+        Bitmap paddedBitmap = Bitmap.createBitmap(targetSize, targetSize, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(paddedBitmap);
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        canvas.drawBitmap(icon, m, paint);
+        return paddedBitmap;
+    }
+
+    /**
+     * @return The ideal size of the media image.
+     */
+    public static int getIdealMediaImageSize() {
+        return SysUtils.isLowEndDevice() ? LOW_IMAGE_SIZE_PX : HIGH_IMAGE_SIZE_PX;
+    }
+
+    /**
+     * @param icon The icon to be checked.
+     * @return Whether |icon| is suitable as the media image, i.e. bigger than the minimal size.
+     */
+    public static boolean isBitmapSuitableAsMediaImage(Bitmap icon) {
+        return icon != null && icon.getWidth() >= MINIMAL_MEDIA_IMAGE_SIZE_PX
+                && icon.getHeight() >= MINIMAL_MEDIA_IMAGE_SIZE_PX;
+    }
+}

chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationTestTabHolder.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.media.ui;
 
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import android.graphics.Bitmap;
@@ -24,6 +25,7 @@ import org.chromium.media_session.mojom.MediaSessionAction;
 import org.chromium.net.GURLUtils;
 import org.chromium.net.GURLUtilsJni;
 import org.chromium.services.media_session.MediaMetadata;
+import org.chromium.url.GURL;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -73,11 +75,9 @@ public class MediaNotificationTestTabHolder {
 
         when(mTab.getWebContents()).thenReturn(mWebContents);
         when(mTab.getId()).thenReturn(tabId);
-        when(mTab.isIncognito()).thenReturn(false);
-        when(mTab.getTitle()).thenAnswer(invocation -> mTitle);
-        when(mTab.getUrlString()).thenAnswer(invocation -> mUrl);
+        when(mWebContents.isIncognito()).thenReturn(false);
 
-        MediaSessionTabHelper.sOverriddenMediaSession = mMediaSession;
+        MediaSessionHelper.sOverriddenMediaSession = mMediaSession;
         mMediaSessionTabHelper = new MediaSessionTabHelper(mTab);
         mMediaSessionTabHelper.mLargeIconBridge = new TestLargeIconBridge();
 
@@ -91,7 +91,7 @@ public class MediaNotificationTestTabHolder {
 
     public void simulateTitleUpdated(String title) {
         mTitle = title;
-        mMediaSessionTabHelper.mTabObserver.onTitleUpdated(mTab);
+        mMediaSessionTabHelper.mMediaSessionHelper.mWebContentsObserver.titleWasSet(title);
     }
 
     public void simulateFaviconUpdated(Bitmap icon) {
@@ -99,29 +99,40 @@ public class MediaNotificationTestTabHolder {
     }
 
     public void simulateMediaSessionStateChanged(boolean isControllable, boolean isSuspended) {
-        mMediaSessionTabHelper.mMediaSessionObserver.mediaSessionStateChanged(
+        mMediaSessionTabHelper.mMediaSessionHelper.mMediaSessionObserver.mediaSessionStateChanged(
                 isControllable, isSuspended);
     }
 
     public void simulateMediaSessionMetadataChanged(MediaMetadata metadata) {
-        mMediaSessionTabHelper.mMediaSessionObserver.mediaSessionMetadataChanged(metadata);
+        mMediaSessionTabHelper.mMediaSessionHelper.mMediaSessionObserver
+                .mediaSessionMetadataChanged(metadata);
     }
 
     public void simulateMediaSessionActionsChanged(Set<Integer> actions) {
-        mMediaSessionTabHelper.mMediaSessionObserver.mediaSessionActionsChanged(actions);
+        mMediaSessionTabHelper.mMediaSessionHelper.mMediaSessionObserver.mediaSessionActionsChanged(
+                actions);
     }
 
     public void simulateNavigation(String url, boolean isSameDocument) {
         mUrl = url;
 
+        // The following hoop jumping is necessary because loading real GURLs fails under junit.
+        GURL gurl = mock(GURL.class);
+        when(mWebContents.getVisibleUrl()).thenAnswer(invocation -> gurl);
+        GURL gurlOrigin = mock(GURL.class);
+        when(gurl.getOrigin()).thenAnswer(invocation -> gurlOrigin);
+        when(gurlOrigin.getSpec()).thenAnswer(invocation -> url);
+
         NavigationHandle navigation = new NavigationHandle(0 /* navigationHandleProxy */, url,
                 true /* isInMainFrame */, isSameDocument, false /* isRendererInitiated */);
-        mMediaSessionTabHelper.mTabObserver.onDidStartNavigation(mTab, navigation);
+        mMediaSessionTabHelper.mMediaSessionHelper.mWebContentsObserver.didStartNavigation(
+                navigation);
 
         navigation.didFinish(url, false /* isErrorPage */, true /* hasCommitted */,
                 false /* isFragmentNavigation */, false /* isDownload */,
                 false /* isValidSearchFormUrl */, 0 /* pageTransition */, 0 /* errorCode */,
                 200 /* httpStatusCode */);
-        mMediaSessionTabHelper.mTabObserver.onDidFinishNavigation(mTab, navigation);
+        mMediaSessionTabHelper.mMediaSessionHelper.mWebContentsObserver.didFinishNavigation(
+                navigation);
     }
 }

chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationTitleUpdatedTest.java
@@ -40,7 +40,7 @@ public class MediaNotificationTitleUpdatedTest extends MediaNotificationManagerT
     private static final int TAB_ID_2 = 2;
     private static final int THROTTLE_MILLIS = MediaNotificationManager.Throttler.THROTTLE_MILLIS;
     private static final int HIDE_NOTIFICATION_DELAY_MILLIS =
-            MediaSessionTabHelper.HIDE_NOTIFICATION_DELAY_MILLIS;
+            MediaSessionHelper.HIDE_NOTIFICATION_DELAY_MILLIS;
 
     private MediaNotificationTestTabHolder mTabHolder;
 

commit 6ac319a0257d84d9913371186c1ff9584f73008d

Restore default large icon for Android MediaSession notifications.
Regressed in 7bb748368975670b485d9b2c75301eeb32818b34 due to an inverted
condition.

Bug: 1098217
Change-Id: I7e5cc46994e7df4ac66a1bbc2681af679d1ce736
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283938
Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#786339}

components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaSessionHelper.java
@@ -56,7 +56,8 @@ public class MediaSessionHelper implements MediaImageCallback {
     private Bitmap mCurrentMediaImage;
     private String mOrigin;
     private int mPreviousVolumeControlStream = AudioManager.USE_DEFAULT_STREAM_TYPE;
-    private MediaNotificationInfo.Builder mNotificationInfoBuilder;
+    @VisibleForTesting
+    public MediaNotificationInfo.Builder mNotificationInfoBuilder;
     // The fallback title if |mPageMetadata| is null or its title is empty.
     private String mFallbackTitle;
     // Set to true if favicon update callback was called at least once.
@@ -225,7 +226,7 @@ public class MediaSessionHelper implements MediaImageCallback {
                 // delegate will pass a favicon later, show nothing for now; we expect the favicon
                 // to arrive quickly.
                 if (mWebContents.isIncognito()
-                        || (mCurrentMediaImage == null && fetchLargeFaviconImage())) {
+                        || (mCurrentMediaImage == null && !fetchLargeFaviconImage())) {
                     mNotificationInfoBuilder.setDefaultNotificationLargeIcon(
                             R.drawable.audio_playing_square);
                 }

chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationFaviconTest.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.media.ui;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doCallRealMethod;
@@ -28,6 +29,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.media.ui.ChromeMediaNotificationControllerDelegate.ListenerService;
 import org.chromium.chrome.browser.ui.favicon.IconType;
 import org.chromium.chrome.browser.ui.favicon.LargeIconBridge;
+import org.chromium.components.browser_ui.media.MediaNotificationInfo;
 
 /**
  * Test of media notifications to ensure that the favicon is displayed on normal devices and
@@ -155,8 +157,29 @@ public class MediaNotificationFaviconTest extends MediaNotificationTestBase {
         assertEquals(mFavicon, getDisplayedIcon());
     }
 
+    @Test
+    public void testWillReturnLargeIcon() {
+        mTabHolder.simulateFaviconUpdated(mFavicon);
+        mTabHolder.mMediaSessionTabHelper.mLargeIconBridge = new TestLargeIconBridge();
+
+        mTabHolder.simulateMediaSessionStateChanged(true, false);
+        assertEquals(0, getCurrentNotificationInfo().defaultNotificationLargeIcon);
+    }
+
+    @Test
+    public void testNoLargeIcon() {
+        mTabHolder.simulateFaviconUpdated(null);
+        mTabHolder.simulateMediaSessionStateChanged(true, false);
+        assertNotEquals(0, getCurrentNotificationInfo().defaultNotificationLargeIcon);
+    }
+
     private Bitmap getDisplayedIcon() {
         return mTabHolder.mMediaSessionTabHelper.mMediaSessionHelper.mFavicon;
+    }
+
+    private MediaNotificationInfo getCurrentNotificationInfo() {
+        return mTabHolder.mMediaSessionTabHelper.mMediaSessionHelper.mNotificationInfoBuilder
+                .build();
     }
 
     @Override