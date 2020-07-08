ed67c19ad4039f6271aec8292138ce6069437475
Tsuyoshi Horo <horo@chromium.org>
2020-07-08 05:32:48 +0000

    Disable org.chromium.chrome.test.smoke.ChromeSmokeTest
    Bug: 970083, 1103107
    TBR=agrieve@chromium.org,aluo@chromium.org,tiborg@chromium.org
    
    Change-Id: Ic4abb6624fa8ea12d430ecadf9716c63d5ac6874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286073
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786173}

--- chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeSmokeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeSmokeTest.java
@@ -18,6 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.test.pagecontroller.rules.ChromeUiApplicationTestRule;
 import org.chromium.chrome.test.pagecontroller.utils.IUi2Locator;
 import org.chromium.chrome.test.pagecontroller.utils.Ui2Locators;
@@ -43,6 +44,7 @@ public class ChromeSmokeTest {
     }
 
     @Test
+    @DisabledTest(message = "https://crbug.com/1103107")
     public void testHello() {
         Context context = InstrumentationRegistry.getContext();
         final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DATA_URL));
