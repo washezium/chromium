affcc770263053954f4629c32d2eb6e6d22723d0
Kim-Anh Tran <kimanh@chromium.org>
2020-07-08 09:59:22 +0000

    Allow interrupts to main thread when calling getPossibleBreakpoints()
    This allows DevTools to retrieve possible breakpoints (and thus
    to set breakpoints) while the main thread is blocking.
    
    Bug: chromium:1076337
    Change-Id: Ia86d1c59bc3036edc334302eb739d5a113a0f55e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284485
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786215}

--- content/browser/devtools/devtools_session.cc
+++ content/browser/devtools/devtools_session.cc
@@ -27,6 +27,7 @@ namespace {
 // TODO(petermarshall): find a way to share this.
 bool ShouldSendOnIO(crdtp::span<uint8_t> method) {
   static auto* kEntries = new std::vector<crdtp::span<uint8_t>>{
+      crdtp::SpanFrom("Debugger.getPossibleBreakpoints"),
       crdtp::SpanFrom("Debugger.getStackTrace"),
       crdtp::SpanFrom("Debugger.pause"),
       crdtp::SpanFrom("Debugger.removeBreakpoint"),
