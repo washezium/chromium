9eb03cdd5310cbea0bcdd4dc4c0256d6e1042b5c
Alex Chau <alexchau@chromium.org>
2020-07-13 21:04:57 +0000

    Implement platform specific code for Nearby Connections
    - Nearby Connections is imported to Chrome in parent CL
      (https://crrev.com/c/2214535)
    - The library will only run in utility process (services/sharing/nearby)
    - This CL implements the platform specific portion of the library,
      for classes like Executor, Mutex, ConditionVariable etc.
    - ScopedAllowBaseSyncPrimitives is used for allowing blocking code when
      using Executor - only run in utility process
    - shared_ptr is used by Nearby Connections API, which requires platform
      specific portion to return shared_ptr
    - Dependencies
      - absl dependency is from Nearby Connections
      - base is used to implement platform specific portion of the library
      - test is for unit tests
    - Design doc: go/nearby-chrome
    
    Bug: 1091190
    Change-Id: I9df6cac80563e1d7e639b7153c6824d0dc88bc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228543
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787899}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -758,9 +758,11 @@ _BANNED_CPP_FUNCTIONS = (
         'std::shared_ptr should not be used. Use scoped_refptr instead.',
       ),
       True,
-      [_THIRD_PARTY_EXCEPT_BLINK,
-       '^third_party/blink/renderer/core/typed_arrays/array_buffer/' +
-         'array_buffer_contents\.(cc|h)'],
+      ['^third_party/blink/renderer/core/typed_arrays/array_buffer/' +
+         'array_buffer_contents\.(cc|h)',
+       # Needed for interop with third-party library
+       'chrome/services/sharing/nearby/',
+       _THIRD_PARTY_EXCEPT_BLINK],  # Not an error in third_party folders.
     ),
     (
       r'/\bstd::weak_ptr\b',
