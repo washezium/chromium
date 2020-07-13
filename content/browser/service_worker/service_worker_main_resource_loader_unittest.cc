3b769afbe6858acaeafa2466d5a01d75c599586c
Kenichi Ishibashi <bashi@chromium.org>
2020-07-13 22:50:56 +0000

    Ensure main script response is available in ServiceWorkerMainResourceLoaderTest
    Before this CL, tests in ServiceWorkerMainResourceLoaderTest
    implicitly depended on a specific ordering of task executions
    which isn't always guaranteed. This CL removes such an implicit
    dependency by waiting for the main script response is received
    before calling ServiceWorkerMainResourceLoader::StartRequest().
    
    Details:
    ServiceWorkerVersion::SetMainScriptResponse() should be called
    before ServiceWorkerMainResourceLoader::StartResponse(). This is
    satisfied just because
    ServiceWorkerInstalledScriptsSender::OnStarted() is called
    synchronously in
    ServiceWorkerInstalledScriptReader::OnReadResponseHeadComplete().
    However, there is no guarantee that OnStarted() is called
    synchronously, especially after ServiceWorkerInstalledScriptReader
    starts using mojo version of reader. This was found in WIP CL [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2282552/1/content/browser/service_worker/service_worker_installed_script_reader.cc#183
    
    Bug: 1055677
    Change-Id: I16fca7c04d3b867ca00e405db7329d2834c2d598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291790
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787940}

--- content/browser/service_worker/service_worker_main_resource_loader_unittest.cc
+++ content/browser/service_worker/service_worker_main_resource_loader_unittest.cc
@@ -212,6 +212,10 @@ class FetchEventServiceWorker : public FakeServiceWorker {
 
   void SetResponseTime(base::Time time) { response_time_ = time; }
 
+  void WaitForTransferInstalledScript() {
+    embedded_worker_instance_client_->WaitForTransferInstalledScript();
+  }
+
  protected:
   void DispatchFetchEventForMainResource(
       blink::mojom::DispatchFetchEventParamsPtr params,
@@ -431,6 +435,21 @@ class ServiceWorkerMainResourceLoaderTest : public testing::Test {
     service_worker_ =
         helper_->AddNewPendingServiceWorker<FetchEventServiceWorker>(
             helper_.get(), client);
+
+    // Wait for main script response is set to |version| because
+    // ServiceWorkerMainResourceLoader needs the main script response to
+    // create a response. The main script response is set when the first
+    // TransferInstalledScript().
+    {
+      base::Optional<blink::ServiceWorkerStatusCode> status;
+      base::RunLoop loop;
+      version_->StartWorker(
+          ServiceWorkerMetrics::EventType::UNKNOWN,
+          ReceiveServiceWorkerStatus(&status, loop.QuitClosure()));
+      loop.Run();
+      ASSERT_EQ(blink::ServiceWorkerStatusCode::kOk, status.value());
+      service_worker_->WaitForTransferInstalledScript();
+    }
   }
 
   ServiceWorkerRegistry* registry() { return helper_->context()->registry(); }
@@ -504,7 +523,7 @@ class ServiceWorkerMainResourceLoaderTest : public testing::Test {
     EXPECT_FALSE(info.load_timing.service_worker_fetch_start.is_null());
     EXPECT_FALSE(
         info.load_timing.service_worker_respond_with_settled.is_null());
-    EXPECT_LT(info.load_timing.service_worker_start_time,
+    EXPECT_LE(info.load_timing.service_worker_start_time,
               info.load_timing.service_worker_ready_time);
     EXPECT_LE(info.load_timing.service_worker_ready_time,
               info.load_timing.service_worker_fetch_start);
