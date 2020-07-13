51f9d25b75c3744c0d7c361911c73133c3a7f5fc
spdonghao <spdonghao@chromium.org>
2020-07-13 20:58:29 +0000

    Re-enable InstantStartTest#testFeedPlaceholderVisibility.
    Bug: 1097003
    Change-Id: I88d396f8217edd9b073a06c353dc1fb5fce423d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290255
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787894}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
@@ -52,7 +52,6 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerChromePhone;
@@ -699,7 +698,6 @@ public class InstantStartTest {
 
     @Test
     @SmallTest
-    @FlakyTest(message = "crbug.com/1097003")
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     @EnableFeatures({ChromeFeatureList.TAB_SWITCHER_ON_RETURN + "<Study,",
             ChromeFeatureList.START_SURFACE_ANDROID + "<Study"})
