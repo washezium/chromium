bf930dcefe4050b6a5c392cc15a47027a379f8b1
Katie D <katie@chromium.org>
2020-07-15 18:49:43 +0000

    Updates reader mode strings for omnibox icon tooltip and menu.
    This matches UX spec for these reader mode strings.
    
    AX-Relnotes: n/a
    Bug: 1041918
    Change-Id: I12c1e54a684c42b655581851a15fc45c4850a9c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292695
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788733}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -822,7 +822,7 @@ void AppMenuModel::Build() {
                 ->GetActiveWebContents()
                 ->GetLastCommittedURL())) {
       // Show the menu option if we are on a distilled page.
-      AddItemWithStringId(IDC_DISTILL_PAGE, IDS_DISTILL_PAGE);
+      AddItemWithStringId(IDC_DISTILL_PAGE, IDS_EXIT_DISTILLED_PAGE);
     } else if (dom_distiller::ShowReaderModeOption(
                    browser_->profile()->GetPrefs())) {
       // Show the menu option if the page is distillable.
