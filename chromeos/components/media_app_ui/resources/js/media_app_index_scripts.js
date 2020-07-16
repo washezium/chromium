21cfcd466c1caf8f34cf6bc4ec3fb7b84c042e66
Oleh Lamzin <lamzin@google.com>
2020-07-16 13:33:34 +0000

    [Media SWA] Move message_pipe.js to components/
    Move message_pipe.js to component, so it can be reusable for
    Media App and Telemetry Extension.
    
    Bug: b:159927590
    
    Change-Id: I9ea31e3b8c43ae80e7adeef5958c4ac6411fa376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244251
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789026}

--- chromeos/components/media_app_ui/resources/js/media_app_index_scripts.js
+++ chromeos/components/media_app_ui/resources/js/media_app_index_scripts.js
@@ -4,6 +4,6 @@
 
 /** @fileoverview Concatenation of the JS files we use in index.html. */
 
-// <include src="message_pipe.js">
+// <include src="../../../system_apps/public/js/message_pipe.js">
 // <include src="message_types.js">
 // <include src="launch.js">
