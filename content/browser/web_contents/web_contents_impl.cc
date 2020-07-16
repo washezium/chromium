f998816347be79620d07a38aadb5e3f2cdcf06ca
David Van Cleve <davidvc@chromium.org>
2020-07-16 19:41:13 +0000

    content: Enforce precondition that UA overrides must be valid headers
    There are some cases (e.g. arbitrary UA overrides from Android WebView)
    where clients can set UA header values that are not valid header values
    in the sense of containing no newlines and no carriage returns. This is
    causing a CHECK failure in net::HttpRequestHeaders (previously, the
    requirement was implicitly specified by a DCHECK; the DCHECK was
    upgraded to a CHECK in crrev.com/c/2270354 because allowing setting
    invalid headers can be a security vulnerability if input is untrusted).
    
    This CL changes WebContents to
    - document the precondition (previously implicit) that UA string
    overrides must, if nonempty, be valid header values
    - enforce this with a CHECK (since it is security-relevant: if we permit
    invalid header values, callers can add arbitrary protected headers).
    
    Bug: 1105745
    Change-Id: I349baef33d1f010d5850c7d91453003d4e113fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299528
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789157}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -145,6 +145,7 @@
 #include "net/base/url_util.h"
 #include "net/http/http_cache.h"
 #include "net/http/http_transaction_factory.h"
+#include "net/http/http_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "ppapi/buildflags/buildflags.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
@@ -1407,6 +1408,11 @@ void WebContentsImpl::SetUserAgentOverride(
 
   if (GetUserAgentOverride() == ua_override)
     return;
+
+  // This is a CHECK because failing this can be security-relevant; see the
+  // comment on net::HttpUtil::IsValidHeaderValue.
+  if (!ua_override.ua_string_override.empty())
+    CHECK(net::HttpUtil::IsValidHeaderValue(ua_override.ua_string_override));
 
   should_override_user_agent_in_new_tabs_ = override_in_new_tabs;
 
