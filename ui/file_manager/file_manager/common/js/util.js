bc46d348d36eba01f1fc308335a648cb93a3f56a
Omid Tourzan <oto@chromium.org>
2020-07-20 18:20:03 +0000

    [files-progress] Add util function to check transfer details flag.
    Bug: 953308
    Change-Id: Id81ab64bb319cc6be2e9aae2dbdb6bf1a04550c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306491
    Commit-Queue: Omid Tourzan <oto@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790019}

--- ui/file_manager/file_manager/common/js/util.js
+++ ui/file_manager/file_manager/common/js/util.js
@@ -1418,6 +1418,14 @@ util.isZipNoNacl = () => {
 };
 
 /**
+ * Returns true if transfer details flag is enabled.
+ * @return {boolean}
+ */
+util.isTransferDetailsEnabled = () => {
+  return loadTimeData.getBoolean('FILES_TRANSFER_DETAILS_ENABLED');
+};
+
+/**
  * Retrieves all entries inside the given |rootEntry|.
  * @param {!DirectoryEntry} rootEntry
  * @param {function(!Array<!Entry>)} entriesCallback Called when some chunk of
