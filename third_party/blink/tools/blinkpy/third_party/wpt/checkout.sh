d1b513c439ac10c6d391df0ec0e7b292e6ecae6b
Robert Ma <robertma@chromium.org>
2020-07-20 18:23:09 +0000

    Roll WPT tools to 635bf0a5f8fd80f4bbc8b6bef0046a64c9a55cf7
    This roll fixes the flakiness introduced in https://crrev.com/c/2304298
    where some test changes needed accompanied tools change (#24617).
    
    All expectations/baselines for fetch/api/abort/general.any.js were
    recreated in this roll.
    
    Commits in wpt tools/ rolled in this change:
    
    0d31626e52 Part 16 - Update web-platform tests to support unrooted android devices using mozdevice 4.0.0, ?
    0fe99651ed Python 3: Port three files in fetch/ (#24617)
    b448d547b6 Fix wpt rev-list failure-handling in epochs_update.sh (#24560)
    d301b89a96 [wptserve] Allow more headers in Python 3 (#24619)
    370f9d815f [lint] Implement EarlyTestdriverVendor lint (#24608)
    165779e785 [wptserve] Make Request.GET.get return a value (#24614)
    00b7aebbbc Update psutil from 5.7.0 to 5.7.2 (#24605)
    b2a666d935 Update mozrunner from 7.8.0 to 8.0.0 (#24579)
    f10f2ba614 Add Servo dependency to docker.
    
    Bug: 1106861
    Change-Id: If161ef2f6f88fbaa62e05bc28fe8abac1578d3b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306249
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790023}

--- third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
+++ third_party/blink/tools/blinkpy/third_party/wpt/checkout.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 TARGET_DIR=$DIR/wpt
 REMOTE_REPO="https://github.com/web-platform-tests/wpt.git"
-WPT_HEAD=149ce86ecb408e9df28e1f55c236ae4388e77cc0
+WPT_HEAD=635bf0a5f8fd80f4bbc8b6bef0046a64c9a55cf7
 
 function clone {
   # Remove existing repo if already exists.
