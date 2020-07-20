8e827fbf321e99b3c960b426d1d3eeee78f48442
Xi Han <hanxi@google.com>
2020-07-20 21:25:57 +0000

    [Instant Start] Make more StartSurface tests work in Instant Start.
    Bug: 1103838, 1092642, 1076274
    Change-Id: I6bf58ef38c834ce15fae04f0743f646fcb98970f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290755
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790119}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -250,11 +250,6 @@ public class StartSurfaceTest {
 
         TabUiTestHelper.createTabs(cta, true, 1);
         TabUiTestHelper.verifyTabModelTabCount(cta, 1, 1);
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
         TabUiTestHelper.enterTabSwitcher(cta);
         if (!isInstantReturn()) {
             // TODO(crbug.com/1076274): fix toolbar to make incognito switch part of the view.
@@ -310,11 +305,7 @@ public class StartSurfaceTest {
 
         TabUiTestHelper.createTabs(cta, true, 1);
         TabUiTestHelper.verifyTabModelTabCount(cta, 1, 1);
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
+
         TabUiTestHelper.enterTabSwitcher(cta);
         if (!isInstantReturn()) {
             // TODO(crbug.com/1076274): fix toolbar to make incognito switch part of the view.
@@ -406,14 +397,15 @@ public class StartSurfaceTest {
             fail("Failed to tap 'more tabs' " + e.toString());
         }
         onViewWaiting(withId(R.id.secondary_tasks_surface_view));
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
 
         pressBack();
         onViewWaiting(withId(R.id.primary_tasks_surface_view));
+
+        if (isInstantReturn()) {
+            // TODO(crbug.com/1092642): Fix androidx.test.espresso.PerformException issue when
+            // performing a single click on position: 0. See code below.
+            return;
+        }
 
         OverviewModeBehaviorWatcher hideWatcher =
                 TabUiTestHelper.createOverviewHideWatcher(mActivityTestRule.getActivity());
@@ -473,14 +465,15 @@ public class StartSurfaceTest {
             fail("Failed to tap 'more tabs' " + e.toString());
         }
         onViewWaiting(withId(R.id.secondary_tasks_surface_view));
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
 
         pressBack();
         onViewWaiting(withId(R.id.primary_tasks_surface_view));
+
+        if (isInstantReturn()) {
+            // TODO(crbug.com/1092642): Fix androidx.test.espresso.PerformException issue when
+            // performing a single click on position: 0. See code below.
+            return;
+        }
 
         OverviewModeBehaviorWatcher hideWatcher =
                 TabUiTestHelper.createOverviewHideWatcher(mActivityTestRule.getActivity());
@@ -545,11 +538,6 @@ public class StartSurfaceTest {
         }
         onViewWaiting(withId(R.id.secondary_tasks_surface_view));
 
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
         pressBack();
         onViewWaiting(withId(R.id.primary_tasks_surface_view));
 
@@ -635,13 +623,15 @@ public class StartSurfaceTest {
             // Single surface is shown as homepage. Exit in order to get into tab switcher later.
             pressBack();
         }
-        if (isInstantReturn()) {
-            // TODO(crbug.com/1076274): fix toolbar to avoid wrongly focusing on the toolbar
-            // omnibox.
-            return;
-        }
+
         TabUiTestHelper.enterTabSwitcher(mActivityTestRule.getActivity());
         onViewWaiting(allOf(withId(R.id.secondary_tasks_surface_view), isDisplayed()));
+
+        if (isInstantReturn()) {
+            // TODO(crbug.com/1092642): Fix androidx.test.espresso.PerformException issue when
+            // performing a single click on position: 0. See code below.
+            return;
+        }
 
         OverviewModeBehaviorWatcher hideWatcher =
                 TabUiTestHelper.createOverviewHideWatcher(mActivityTestRule.getActivity());
