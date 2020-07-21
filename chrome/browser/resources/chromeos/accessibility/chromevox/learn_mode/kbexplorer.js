cb63f3b2efa4ec4a83ecf73845bcf2d53b4d2324
David Tseng <dtseng@chromium.org>
2020-07-21 03:51:29 +0000

    Revert "Prevent repeated keys from providing output in learn mode"
    This reverts commit 6c45baae007bfe7dbfd838fe6a58639f9dfda573.
    
    Reason for revert: <INSERT REASONING HERE>
    Need to stop propagation of key events even if they are repeated.
    
    Original change's description:
    > Prevent repeated keys from providing output in learn mode
    > 
    > R=​akihiroota@chromium.org
    > 
    > Fixed: 1106462
    > AX-Relnotes: only announce a modifier once, when holding down modifiers like Search in ChromeVox Learn Mode.
    > Change-Id: Iaa9de466462450ccef889a2b789b0c150127c814
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308489
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790193}
    
    TBR=dtseng@chromium.org,akihiroota@chromium.org
    
    Change-Id: I4d2646397a765a6e78b11dd2ebdd777ed45be8ef
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309563
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790223}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/kbexplorer.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/kbexplorer.js
@@ -72,9 +72,6 @@ KbExplorer = class {
       ChromeVox.tts.stop();
     }
     KbExplorer.keydownWithoutKeyupEvents_.add(evt.keyCode);
-    if (evt.repeat) {
-      return false;
-    }
     ChromeVox.tts.speak(
         KeyUtil.getReadableNameForKeyCode(evt.keyCode),
         window.backgroundWindow.QueueMode.QUEUE,
