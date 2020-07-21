66d3f14b7af8bde3d074b7d34082dc15bb83d12d
Jimmy Gong <jimmyxgong@chromium.org>
2020-07-21 23:17:23 +0000

    Rename Print management app to Print jobs
    - Rename the app name from "Print Management App" to "Print Jobs" for
      consistency with the settings entry point and app title.
    
    (cherry picked from commit 58aed91e69cb996e208910062dfe8add8834f311)
    
    Bug: 1106142
    Change-Id: I0dd0aaecefe03f52b46a0154a090ecdf417b1ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303970
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#789626}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311033
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4183@{#823}
    Cr-Branched-From: 740e9e8a40505392ba5c8e022a8024b3d018ca65-refs/heads/master@{#782793}

--- chromeos/components/print_management/print_management_ui.cc
+++ chromeos/components/print_management/print_management_ui.cc
@@ -8,6 +8,7 @@
 #include "base/memory/ptr_util.h"
 #include "chromeos/components/print_management/mojom/printing_manager.mojom.h"
 #include "chromeos/components/print_management/url_constants.h"
+#include "chromeos/components/web_applications/manifest_request_filter.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/grit/chromeos_print_management_resources.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
@@ -119,6 +120,9 @@ PrintManagementUI::PrintManagementUI(
   html_source->SetDefaultResource(IDR_PRINT_MANAGEMENT_INDEX_HTML);
 
   AddPrintManagementStrings(html_source.get());
+  web_app::SetManifestRequestFilter(html_source.get(),
+                                    IDR_PRINT_MANAGEMENT_MANIFEST,
+                                    IDS_PRINT_MANAGEMENT_APP_NAME);
 
   if (base::FeatureList::IsEnabled(chromeos::features::kScanningUI)) {
     html_source->AddResourcePath("scanning.html", IDR_SCANNING_HTML);
