2e74d1769330a5692cefe671e5af1b1f1210b1d6
Boris Sazonov <bsazonov@chromium.org>
2020-07-21 07:12:28 +0000

    [Android] Fix a comment in AndroidSyncSettingsTestUtils
    No behavior change. This is a follow-up to https://crrev.com/c/2289972
    that clarifies a comment in AndroidSyncSettingsTestUtils.
    
    Bug: 1093998
    Change-Id: I5eba992467c15e26de67cd5ea4f411ddb033be9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308151
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790282}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/AndroidSyncSettingsTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/AndroidSyncSettingsTestUtils.java
@@ -28,8 +28,9 @@ public class AndroidSyncSettingsTestUtils {
     @VisibleForTesting
     public static void setUpAndroidSyncSettingsForTesting(SyncContentResolverDelegate delegate) {
         delegate.setMasterSyncAutomatically(true);
-        // AndroidSyncSettings ctor usually gets sync account from IdentityManager, which may have
-        // state left by previous tests. To avoid this, pass null account explicitly.
+        // Explicitly pass null account to AndroidSyncSettings ctor. Normally, AndroidSyncSettings
+        // ctor uses IdentityManager to get the sync account, but some native tests call this method
+        // before profiles are initialized (when IdentityManager doesn't exist yet).
         AndroidSyncSettings.overrideForTests(new AndroidSyncSettings(delegate, null, null));
     }
 }
