6dc4bb48d9c96ab33b1a605c97196769d9549ffe
Asami Doi <asamidoi@chromium.org>
2020-07-22 08:54:04 +0000

    [M85] Fix ServiceWorker.FetchEvent.QueuingTime metric (CheckOfflineCapability)
    ServiceWorker.FetchEvent.QueuingTime is used for the Finch experiment
    (CheckOfflineCapability), but it always records a minimum value (0.001)
    because the order of a subtraction was wrong. The result of a
    subtraction became a minus value and it was rounded to a minimum value.
    
    This CL fixes the order in a subtraction.
    
    (cherry picked from commit 832f62bc3f414dd6be2442c6f581aa27464216d5)
    
    Bug: 965802, 1106693
    Change-Id: I93b60f301a16caef355e221712372c8e5bcde56f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303236
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#789810}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310518
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/4183@{#845}
    Cr-Branched-From: 740e9e8a40505392ba5c8e022a8024b3d018ca65-refs/heads/master@{#782793}

--- third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
@@ -2412,7 +2412,7 @@ void ServiceWorkerGlobalScope::NoteRespondedToFetchEvent(
 void ServiceWorkerGlobalScope::RecordQueuingTime(base::TimeTicks created_time) {
   base::UmaHistogramMediumTimes(
       "ServiceWorker.FetchEvent.QueuingTime",
-      created_time - base::TimeTicks::Now());
+      base::TimeTicks::Now() - created_time);
 }
 
 }  // namespace blink
