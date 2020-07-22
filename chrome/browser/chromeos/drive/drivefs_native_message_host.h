0b8fe9931307b3ed84b2feb25ebfa4d532ccf7df
Austin Tankiang <austinct@chromium.org>
2020-07-22 02:52:43 +0000

    Use bidirectional native message api for DriveFS
    This ports the existing native message functionality to use
    CreateNativeHostSession() if the corresponding flag is enabled.
    
    Bug: 1105319
    Change-Id: Iadb4544e39ebc818f5e0309dd8e98b5fd14c801e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306508
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790694}

--- chrome/browser/chromeos/drive/drivefs_native_message_host.h
+++ chrome/browser/chromeos/drive/drivefs_native_message_host.h
@@ -7,8 +7,11 @@
 
 #include <memory>
 
+#include "chromeos/components/drivefs/mojom/drivefs.mojom.h"
 #include "content/public/browser/browser_context.h"
 #include "extensions/browser/api/messaging/native_message_host.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
 
 namespace drive {
 
@@ -20,6 +23,16 @@ extern const size_t kDriveFsNativeMessageHostOriginsSize;
 
 std::unique_ptr<extensions::NativeMessageHost> CreateDriveFsNativeMessageHost(
     content::BrowserContext* browser_context);
+
+std::unique_ptr<extensions::NativeMessageHost>
+CreateDriveFsInitiatedNativeMessageHost(
+    mojo::PendingReceiver<drivefs::mojom::NativeMessagingPort>
+        extension_receiver,
+    mojo::PendingRemote<drivefs::mojom::NativeMessagingHost> drivefs_remote);
+
+std::unique_ptr<extensions::NativeMessageHost>
+CreateDriveFsNativeMessageHostForTesting(
+    drivefs::mojom::DriveFs* drivefs_for_testing);
 
 }  // namespace drive
 
