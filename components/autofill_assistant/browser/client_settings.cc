7228039446b61c237cdbd70a7f913806b5006356
sandromaggi <sandromaggi@google.com>
2020-07-23 10:00:48 +0000

    [Autofill Assistant] Don't reset overlay_image if it's missing
    Before this change, a missing entry of |overlay_image| would reset it. Now
    to unset it one needs to add an invalid (e.g. empty) setting for it.
    
    Bug: b/161870830
    Change-Id: I9a7fa1b11fe437071a7e4bbea11b44fb95f3b1c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315858
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#791192}

--- components/autofill_assistant/browser/client_settings.cc
+++ components/autofill_assistant/browser/client_settings.cc
@@ -78,16 +78,12 @@ void ClientSettings::UpdateFromProto(const ClientSettingsProto& proto) {
     tap_shutdown_delay =
         base::TimeDelta::FromMilliseconds(proto.tap_shutdown_delay_ms());
   }
-  if (proto.has_overlay_image() &&
-      IsValidOverlayImageProto(proto.overlay_image())) {
-    overlay_image = proto.overlay_image();
-  } else {
-    overlay_image.reset();
-  }
-  if (proto.has_integration_test_settings()) {
-    integration_test_settings = proto.integration_test_settings();
-  } else {
-    integration_test_settings.reset();
+  if (proto.has_overlay_image()) {
+    if (IsValidOverlayImageProto(proto.overlay_image())) {
+      overlay_image = proto.overlay_image();
+    } else {
+      overlay_image.reset();
+    }
   }
   if (proto.has_talkback_sheet_size_fraction()) {
     talkback_sheet_size_fraction = proto.talkback_sheet_size_fraction();
@@ -99,6 +95,13 @@ void ClientSettings::UpdateFromProto(const ClientSettingsProto& proto) {
     } else {
       back_button_settings.reset();
     }
+  }
+
+  // Test only settings.
+  if (proto.has_integration_test_settings()) {
+    integration_test_settings = proto.integration_test_settings();
+  } else {
+    integration_test_settings.reset();
   }
 }
 
