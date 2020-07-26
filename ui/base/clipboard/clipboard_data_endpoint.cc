db0cf440fc47958bf9f41750937424d1a1542e2b
Aya ElAttar <ayaelattar@google.com>
2020-07-26 15:07:40 +0000

    Change ClipboardDataEndpoint to support virtual machines
    Changed ClipboardDataEndpoint to support virtual machines,
    like Crostini, PluginVM, ARC++ as potential sources
    or destinations of the clipboard data.
    
    Bug: 1103217
    Change-Id: I3ff721a8fbc685adfb5779b1edb579da4062ff24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315030
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791647}

--- ui/base/clipboard/clipboard_data_endpoint.cc
+++ ui/base/clipboard/clipboard_data_endpoint.cc
@@ -4,11 +4,19 @@
 
 #include "ui/base/clipboard/clipboard_data_endpoint.h"
 
+#include "base/check_op.h"
+#include "base/optional.h"
 #include "url/origin.h"
 
 namespace ui {
 
-ClipboardDataEndpoint::ClipboardDataEndpoint(const GURL& url) : url_(url) {}
+ClipboardDataEndpoint::ClipboardDataEndpoint(const GURL& url)
+    : type_(EndpointType::kUrl), url_(url) {}
+
+ClipboardDataEndpoint::ClipboardDataEndpoint(EndpointType type)
+    : type_(type), url_(base::nullopt) {
+  DCHECK_NE(type, EndpointType::kUrl);
+}
 
 ClipboardDataEndpoint::ClipboardDataEndpoint(
     const ClipboardDataEndpoint& other) = default;
@@ -16,15 +24,9 @@ ClipboardDataEndpoint::ClipboardDataEndpoint(
 ClipboardDataEndpoint::ClipboardDataEndpoint(ClipboardDataEndpoint&& other) =
     default;
 
-ClipboardDataEndpoint& ClipboardDataEndpoint::operator=(
-    const ClipboardDataEndpoint& other) = default;
-
-ClipboardDataEndpoint& ClipboardDataEndpoint::operator=(
-    ClipboardDataEndpoint&& other) = default;
-
 bool ClipboardDataEndpoint::operator==(
     const ClipboardDataEndpoint& other) const {
-  return url_ == other.url_;
+  return url_ == other.url_ && type_ == other.type_;
 }
 
 ClipboardDataEndpoint::~ClipboardDataEndpoint() = default;
