706a5c51cfce0f678470510d3b91b816be957534
Richard Knoll <knollr@chromium.org>
2020-07-26 02:30:29 +0000

    [Nearby] Start discovery on discovery page
    This calls the DiscoveryManager interface from JS and listens for
    discovered and lost devices to update the device list. The selected
    device is then passed through to the confirmation-page.
    
    Bug: 1103190
    Change-Id: I9296e5449533eb869fd9f20fe442d4ac1bc05a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315862
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791591}

--- chrome/test/data/webui/nearby_share/nearby_discovery_page_test.js
+++ chrome/test/data/webui/nearby_share/nearby_discovery_page_test.js
@@ -8,7 +8,7 @@ import 'chrome://nearby/nearby_discovery_page.js';
 
 import {setDiscoveryManagerForTesting} from 'chrome://nearby/discovery_manager.js';
 
-import {assertEquals} from '../chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 
 import {FakeConfirmationManagerRemote, FakeDiscoveryManagerRemote} from './fake_mojo_interfaces.js';
 
@@ -16,7 +16,55 @@ suite('DiscoveryPageTest', function() {
   /** @type {!NearbyDiscoveryPageElement} */
   let discoveryPageElement;
 
+  /** @type {!FakeDiscoveryManagerRemote} */
+  let discoveryManager;
+
+  /** @type {!number} Next device id to be used. */
+  let nextId = 0;
+
+  /**
+   * Get the list of device names that are currently shown.
+   * @return {!Array<string>}
+   */
+  function getDeviceNames() {
+    /** @type {!Array<string>} */
+    const deviceNames = [];
+    for (const device of discoveryPageElement.$$('#device-list').children) {
+      if (device.is === 'nearby-device') {
+        deviceNames.push(device.$$('#name').textContent);
+      }
+    }
+    return deviceNames;
+  }
+
+  /**
+   * @param {!string} name Device name
+   * @return {!nearbyShare.mojom.ShareTarget}
+   */
+  function createShareTarget(name) {
+    return {
+      id: {high: 0, low: nextId++},
+      name,
+      type: nearbyShare.mojom.ShareTargetType.kPhone,
+    };
+  }
+
+  /**
+   * Creates a share target and sends it to the WebUI.
+   * @return {!Promise<nearbyShare.mojom.ShareTarget>}
+   */
+  async function setupShareTarget() {
+    /** @type {!nearbyShare.mojom.ShareTargetListenerRemote} */
+    const listener = await discoveryManager.whenCalled('startDiscovery');
+    const shareTarget = createShareTarget('Device Name');
+    listener.onShareTargetDiscovered(shareTarget);
+    await listener.$.flushForTesting();
+    return shareTarget;
+  }
+
   setup(function() {
+    discoveryManager = new FakeDiscoveryManagerRemote();
+    setDiscoveryManagerForTesting(discoveryManager);
     discoveryPageElement = /** @type {!NearbyDiscoveryPageElement} */ (
         document.createElement('nearby-discovery-page'));
     document.body.appendChild(discoveryPageElement);
@@ -31,31 +79,27 @@ suite('DiscoveryPageTest', function() {
   });
 
   test('selects share target with success', async function() {
-    const manager = new FakeDiscoveryManagerRemote();
-    setDiscoveryManagerForTesting(manager);
-
+    const created = await setupShareTarget();
     discoveryPageElement.$$('#next-button').click();
-
-    await manager.whenCalled('selectShareTarget');
+    const selectedId = await discoveryManager.whenCalled('selectShareTarget');
+    assertEquals(created.id.high, selectedId.high);
+    assertEquals(created.id.low, selectedId.low);
   });
 
   test('selects share target with error', async function() {
-    const manager = new FakeDiscoveryManagerRemote();
-    manager.selectShareTargetResult.result =
+    await setupShareTarget();
+    discoveryManager.selectShareTargetResult.result =
         nearbyShare.mojom.SelectShareTargetResult.kError;
-    setDiscoveryManagerForTesting(manager);
 
     discoveryPageElement.$$('#next-button').click();
-
-    await manager.whenCalled('selectShareTarget');
+    await discoveryManager.whenCalled('selectShareTarget');
   });
 
   test('selects share target with confirmation', async function() {
-    const manager = new FakeDiscoveryManagerRemote();
-    manager.selectShareTargetResult.token = 'test token';
-    manager.selectShareTargetResult.confirmationManager =
+    await setupShareTarget();
+    discoveryManager.selectShareTargetResult.token = 'test token';
+    discoveryManager.selectShareTargetResult.confirmationManager =
         new FakeConfirmationManagerRemote();
-    setDiscoveryManagerForTesting(manager);
 
     let eventDetail = null;
     discoveryPageElement.addEventListener('change-page', (event) => {
@@ -64,7 +108,66 @@ suite('DiscoveryPageTest', function() {
 
     discoveryPageElement.$$('#next-button').click();
 
-    await manager.whenCalled('selectShareTarget');
+    await discoveryManager.whenCalled('selectShareTarget');
     assertEquals('confirmation', eventDetail.page);
+  });
+
+  test('starts discovery', async function() {
+    await discoveryManager.whenCalled('startDiscovery');
+  });
+
+  test('shows newly discovered device', async function() {
+    /** @type {!nearbyShare.mojom.ShareTargetListenerRemote} */
+    const listener = await discoveryManager.whenCalled('startDiscovery');
+    const deviceName = 'Device Name';
+
+    listener.onShareTargetDiscovered(createShareTarget(deviceName));
+    await listener.$.flushForTesting();
+
+    assertTrue(getDeviceNames().includes(deviceName));
+  });
+
+  test('shows multiple discovered devices', async function() {
+    /** @type {!nearbyShare.mojom.ShareTargetListenerRemote} */
+    const listener = await discoveryManager.whenCalled('startDiscovery');
+    const deviceName1 = 'Device Name 1';
+    const deviceName2 = 'Device Name 2';
+
+    listener.onShareTargetDiscovered(createShareTarget(deviceName1));
+    listener.onShareTargetDiscovered(createShareTarget(deviceName2));
+    await listener.$.flushForTesting();
+
+    assertTrue(getDeviceNames().includes(deviceName1));
+    assertTrue(getDeviceNames().includes(deviceName2));
+  });
+
+  test('removes lost device', async function() {
+    /** @type {!nearbyShare.mojom.ShareTargetListenerRemote} */
+    const listener = await discoveryManager.whenCalled('startDiscovery');
+    const deviceName = 'Device Name';
+    const shareTarget = createShareTarget(deviceName);
+
+    listener.onShareTargetDiscovered(shareTarget);
+    listener.onShareTargetLost(shareTarget);
+    await listener.$.flushForTesting();
+
+    assertFalse(getDeviceNames().includes(deviceName));
+  });
+
+  test('replaces existing device', async function() {
+    /** @type {!nearbyShare.mojom.ShareTargetListenerRemote} */
+    const listener = await discoveryManager.whenCalled('startDiscovery');
+    const deviceName = 'Device Name';
+    const shareTarget = createShareTarget(deviceName);
+
+    listener.onShareTargetDiscovered(shareTarget);
+    await listener.$.flushForTesting();
+
+    const expectedDeviceCount = getDeviceNames().length;
+
+    listener.onShareTargetDiscovered(shareTarget);
+    await listener.$.flushForTesting();
+
+    assertEquals(expectedDeviceCount, getDeviceNames().length);
   });
 });
