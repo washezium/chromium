fecfa2c17bd8b70f0233c4737fdabf29c04c1588
Adlai Holler <adlai@google.com>
2020-07-27 21:01:10 +0000

    Add staging for https://review.skia.org/305102
    This is part of a larger effort to force users to provide
    GrDirectContext when manipulating texture-backed images.
    
    Bug: skia:104662
    Change-Id: Ic21ba05d0f13d9e597bb3eac49ac818209b1c1c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2313301
    Commit-Queue: Adlai Holler <adlai@google.com>
    Auto-Submit: Adlai Holler <adlai@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/master@{#791980}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -236,6 +236,9 @@ SK_API void SkDebugf_FileLine(const char* file,
 // Staging for https://review.skia.org/304797
 #define SK_IMAGE_FLUSH_LEGACY_API
 
+// Staging for https://review.skia.org/305102
+#define SK_IMAGE_MAKE_FROM_TEXTURE_LEGACY_API
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
