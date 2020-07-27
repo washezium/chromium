706ac3cdb7d74f230de98afc925e47525d22d1ce
My Nguyen <myy@chromium.org>
2020-07-27 21:21:54 +0000

    Add languages_page_metrics_test_cros for closure compile
    Bug: 1000989
    Change-Id: I7ae5d8279cb46866e28a4393d8cdc0d3ae21bcd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315901
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791994}

--- chrome/test/data/webui/settings/languages_page_metrics_test_cros.js
+++ chrome/test/data/webui/settings/languages_page_metrics_test_cros.js
@@ -6,6 +6,7 @@ import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min
 import {LanguagesBrowserProxyImpl, LanguagesMetricsProxyImpl, LanguagesPageInteraction} from 'chrome://settings/lazy_load.js';
 import {CrSettingsPrefs} from 'chrome://settings/settings.js';
 
+import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 import {fakeDataBind} from '../test_util.m.js';
 
 import {getFakeLanguagePrefs} from './fake_language_settings_private.m.js';
@@ -14,21 +15,21 @@ import {TestLanguagesBrowserProxy} from './test_languages_browser_proxy.m.js';
 import {TestLanguagesMetricsProxy} from './test_languages_metrics_proxy.js';
 
 suite('LanguagesPageMetricsChromeOS', function() {
-  /** @type {?LanguageHelper} */
-  let languageHelper = null;
-  /** @type {?SettingsLanguagesPageElement} */
-  let languagesPage = null;
-  /** @type {?LanguagesBrowserProxy} */
-  let browserProxy = null;
-  /** @type {?LanguagesMetricsProxy} */
-  let languagesMetricsProxy = null;
+  /** @type {!LanguageHelper} */
+  let languageHelper;
+  /** @type {!SettingsLanguagesPageElement} */
+  let languagesPage;
+  /** @type {!TestLanguagesBrowserProxy} */
+  let browserProxy;
+  /** @type {!TestLanguagesMetricsProxy} */
+  let languagesMetricsProxy;
 
   suiteSetup(function() {
-    PolymerTest.clearBody();
     CrSettingsPrefs.deferInitialization = true;
   });
 
   setup(function() {
+    document.body.innerHTML = '';
     const settingsPrefs = document.createElement('settings-prefs');
     const settingsPrivate = new FakeSettingsPrivate(getFakeLanguagePrefs());
     settingsPrefs.initialize(settingsPrivate);
@@ -46,7 +47,8 @@ suite('LanguagesPageMetricsChromeOS', function() {
       const languageSettingsPrivate = browserProxy.getLanguageSettingsPrivate();
       languageSettingsPrivate.setSettingsPrefs(settingsPrefs);
 
-      languagesPage = document.createElement('settings-languages-page');
+      languagesPage = /** @type {!SettingsLanguagesPageElement} */ (
+          document.createElement('settings-languages-page'));
 
       // Prefs would normally be data-bound to settings-languages-page.
       languagesPage.prefs = settingsPrefs.prefs;
@@ -58,19 +60,15 @@ suite('LanguagesPageMetricsChromeOS', function() {
     });
   });
 
-  teardown(function() {
-    PolymerTest.clearBody();
-  });
-
   test('records when adding languages', async () => {
-    languagesPage.$.addLanguages.click();
+    languagesPage.$$('#addLanguages').click();
     flush();
 
     await languagesMetricsProxy.whenCalled('recordAddLanguages');
   });
 
   test('records when clicking edit dictionary', async () => {
-    languagesPage.$.spellCheckSubpageTrigger.click();
+    languagesPage.$$('#spellCheckSubpageTrigger').click();
     flush();
 
     assertEquals(
@@ -79,8 +77,8 @@ suite('LanguagesPageMetricsChromeOS', function() {
   });
 
   test('records when disabling translate.enable toggle', async () => {
-    languageHelper.setPrefValue('translate.enabled', true);
-    languagesPage.$.offerTranslateOtherLanguages.click();
+    languagesPage.setPrefValue('translate.enabled', true);
+    languagesPage.$$('#offerTranslateOtherLanguages').click();
     flush();
 
     assertFalse(
@@ -88,16 +86,16 @@ suite('LanguagesPageMetricsChromeOS', function() {
   });
 
   test('records when enabling translate.enable toggle', async () => {
-    languageHelper.setPrefValue('translate.enabled', false);
-    languagesPage.$.offerTranslateOtherLanguages.click();
+    languagesPage.setPrefValue('translate.enabled', false);
+    languagesPage.$$('#offerTranslateOtherLanguages').click();
     flush();
 
     assertTrue(await languagesMetricsProxy.whenCalled('recordToggleTranslate'));
   });
 
   test('records when disabling spell check toggle', async () => {
-    languageHelper.setPrefValue('browser.enable_spellchecking', true);
-    languagesPage.$.enableSpellcheckingToggle.click();
+    languagesPage.setPrefValue('browser.enable_spellchecking', true);
+    languagesPage.$$('#enableSpellcheckingToggle').click();
     flush();
 
     assertFalse(
@@ -105,8 +103,8 @@ suite('LanguagesPageMetricsChromeOS', function() {
   });
 
   test('records when enabling spell check toggle', async () => {
-    languageHelper.setPrefValue('browser.enable_spellchecking', false);
-    languagesPage.$.enableSpellcheckingToggle.click();
+    languagesPage.setPrefValue('browser.enable_spellchecking', false);
+    languagesPage.$$('#enableSpellcheckingToggle').click();
     flush();
 
     assertTrue(
@@ -121,7 +119,7 @@ suite('LanguagesPageMetricsChromeOS', function() {
 
     flush();
 
-    const languagesCollapse = languagesPage.$.languagesCollapse;
+    const languagesCollapse = languagesPage.$$('#languagesCollapse');
     languagesCollapse.opened = true;
 
     const menuButtons = languagesCollapse.querySelectorAll(
@@ -130,7 +128,7 @@ suite('LanguagesPageMetricsChromeOS', function() {
     // Chooses the second language to switch system language,
     // as first language is the default language.
     menuButtons[1].click();
-    const actionMenu = languagesPage.$.menu.get();
+    const actionMenu = languagesPage.$$('#menu').get();
     assertTrue(actionMenu.open);
     const menuItems = actionMenu.querySelectorAll('.dropdown-item');
     for (const item of menuItems) {
@@ -147,7 +145,7 @@ suite('LanguagesPageMetricsChromeOS', function() {
     actionMenu.close();
 
     // Chooses restart button after switching system language.
-    const restartButton = languagesPage.$.restartButton;
+    const restartButton = languagesPage.$$('#restartButton');
     assertTrue(!!restartButton);
     restartButton.click();
 
