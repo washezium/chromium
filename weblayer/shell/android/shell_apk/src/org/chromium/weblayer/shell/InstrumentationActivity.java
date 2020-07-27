7fa750159880268df8fc511fa402060e0caff6ac
Evan Stade <estade@chromium.org>
2020-07-27 18:06:44 +0000

    WebLayer: Test content resizing when soft keyboard shows/hides
    This test aborts if the OSK doesn't seem to work, which can be the case
    when a hardware keyboard is present. I found no better way to determine
    if the soft keyboard is disabled or force it to be enabled. The CQ bots
    appear to run with a soft keyboard and no hard keyboard, so they should
    run the test to completion.
    
    The presence of the OSK is verified by checking the size of the content
    when not in fullscreen, behavior that has always worked. This is because
    the behavior under test is respecting the OSK size when the content *is*
    in fullscreen, so aborting based on fs behavior would not have caught
    this bug (the bug would have made it seem as if there were no OSK).
    
    This is split off from the fix in case the test turns out to be flaky.
    
    Bug: 1107651
    Change-Id: I803a67a4adef0bc4ee19b70a4d2ebd3126cd632f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315425
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791868}

--- weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/InstrumentationActivity.java
+++ weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/InstrumentationActivity.java
@@ -14,6 +14,7 @@ import android.os.StrictMode.VmPolicy;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -24,6 +25,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.weblayer.Browser;
+import org.chromium.weblayer.FullscreenCallback;
 import org.chromium.weblayer.NewTabCallback;
 import org.chromium.weblayer.NewTabType;
 import org.chromium.weblayer.Profile;
@@ -63,6 +65,7 @@ public class InstrumentationActivity extends FragmentActivity {
     private IntentInterceptor mIntentInterceptor;
     private Bundle mSavedInstanceState;
     private TabCallback mRendererCrashListener;
+    private Runnable mExitFullscreenRunnable;
     private boolean mIgnoreRendererCrashes;
     private TabListCallback mTabListCallback;
     private List<Tab> mPreviousTabList = new ArrayList<>();
@@ -257,7 +260,7 @@ public class InstrumentationActivity extends FragmentActivity {
                 if (mTab == null) {
                     setTab(tab);
                 }
-                tab.registerTabCallback(mRendererCrashListener);
+                setTabCallbacks(tab);
             }
 
             @Override
@@ -282,9 +285,50 @@ public class InstrumentationActivity extends FragmentActivity {
             // This happens with session restore enabled.
             assert mBrowser.getTabs().size() == 0;
         } else {
-            mBrowser.getActiveTab().registerTabCallback(mRendererCrashListener);
+            setTabCallbacks(mBrowser.getActiveTab());
             setTab(mBrowser.getActiveTab());
         }
+    }
+
+    private void setTabCallbacks(Tab tab) {
+        tab.registerTabCallback(mRendererCrashListener);
+
+        tab.setFullscreenCallback(new FullscreenCallback() {
+            private int mSystemVisibilityToRestore;
+
+            @Override
+            public void onEnterFullscreen(Runnable exitFullscreenRunnable) {
+                mExitFullscreenRunnable = exitFullscreenRunnable;
+                // This comes from Chrome code to avoid an extra resize.
+                final WindowManager.LayoutParams attrs = getWindow().getAttributes();
+                attrs.flags |= WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                getWindow().setAttributes(attrs);
+
+                View decorView = getWindow().getDecorView();
+                // Caching the system ui visibility is ok for shell, but likely not ok for
+                // real code.
+                mSystemVisibilityToRestore = decorView.getSystemUiVisibility();
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+
+            @Override
+            public void onExitFullscreen() {
+                mExitFullscreenRunnable = null;
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(mSystemVisibilityToRestore);
+
+                final WindowManager.LayoutParams attrs = getWindow().getAttributes();
+                if ((attrs.flags & WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS) != 0) {
+                    attrs.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+                    getWindow().setAttributes(attrs);
+                }
+            }
+        });
     }
 
     private void createUrlBarView() {
