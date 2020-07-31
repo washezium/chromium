3dddb889037fa4cfcd5add5035b23f47e2821c6d
Eric Roman <eroman@chromium.org>
2020-07-31 21:57:55 +0000

    Move some test-only code out of NetLog.
    Change-Id: If23d2017ad8d15b38ffe22e0f51e5ed070def161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332584
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Auto-Submit: Eric Roman <eroman@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793767}

--- net/log/net_log_source.cc
+++ net/log/net_log_source.cc
@@ -57,39 +57,4 @@ base::Value NetLogSource::ToEventParameters() const {
   return SourceEventParametersCallback(*this);
 }
 
-// static
-bool NetLogSource::FromEventParameters(const base::Value* event_params,
-                                       NetLogSource* source) {
-  const base::Value* source_dict = nullptr;
-  int source_id = -1;
-  int source_type = static_cast<int>(NetLogSourceType::COUNT);
-  if (!event_params) {
-    *source = NetLogSource();
-    return false;
-  }
-  source_dict = event_params->FindDictKey("source_dependency");
-  if (!source_dict) {
-    *source = NetLogSource();
-    return false;
-  }
-  base::Optional<int> opt_int;
-  opt_int = source_dict->FindIntKey("id");
-  if (!opt_int) {
-    *source = NetLogSource();
-    return false;
-  }
-  source_id = opt_int.value();
-  opt_int = source_dict->FindIntKey("type");
-  if (!opt_int) {
-    *source = NetLogSource();
-    return false;
-  }
-  source_type = opt_int.value();
-
-  DCHECK_GE(source_id, 0);
-  DCHECK_LT(source_type, static_cast<int>(NetLogSourceType::COUNT));
-  *source = NetLogSource(static_cast<NetLogSourceType>(source_type), source_id);
-  return true;
-}
-
 }  // namespace net
