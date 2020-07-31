cf2d545c86b23d9fc5a23e92668a3738b0bd93e8
Henrique Nakashima <hnakashima@chromium.org>
2020-07-31 18:37:21 +0000

    [Lorenz] Load dependency graph from GS bucket, fallback to localhost
    Currently we only load from localhost. That will not work in production,
    so first try to access a graph from gs://clank-dependency-graphs, then
    fall back to localhost (which cannot GET from the bucket due to CORS).
    
    Bug: 1111056
    Change-Id: Idf06dfeb6eb49047e7c4c54f60c99e156f0387d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327463
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793703}

--- tools/android/dependency_analysis/js/src/class_view.js
+++ tools/android/dependency_analysis/js/src/class_view.js
@@ -2,17 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// For ease of development, we serve our testing data on localhost:8888. This
-// should be changed as we find other ways to serve the assets (eg. user upload
-// or hosted externally).
-const LOCALHOST = 'http://localhost:8888';
-
 import Vue from 'vue';
 import ClassGraphPage from './vue_components/class_graph_page.vue';
+import {loadGraph} from './load_graph.js';
 import * as d3 from 'd3';
 
 document.addEventListener('DOMContentLoaded', () => {
-  d3.json(`${LOCALHOST}/json_graph.txt`).then(data => {
+  loadGraph().then(data => {
     new Vue({
       el: '#class-graph-page',
       render: createElement => createElement(
@@ -24,5 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
           },
       ),
     });
+  }).catch(e => {
+    document.write("Error loading graph.");
   });
 });
