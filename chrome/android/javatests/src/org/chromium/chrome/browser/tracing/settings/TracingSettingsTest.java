99225653881c9f84dcd006e189aeb879f781ec97
Tim Volodine <timvolodine@google.com>
2020-07-31 16:25:14 +0000

    Disable flaky TracingSettingsTest#testRecordTrace on L
    Flaky on Lollipop Phone Tester
    
    BUG=1111816
    NOTRY=true
    TBR=skyostil@chromium.org
    
    Change-Id: Ia1176f4ec57495aaef0dfd107a494c940a533b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332621
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793646}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tracing/settings/TracingSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tracing/settings/TracingSettingsTest.java
@@ -10,6 +10,7 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build.VERSION_CODES;
 import android.support.test.InstrumentationRegistry;
 import android.util.Pair;
 
@@ -146,7 +147,8 @@ public class TracingSettingsTest {
     @MediumTest
     @Feature({"Preferences"})
     @DisabledTest
-    @DisableIf.Build(sdk_is_less_than = 21, message = "crbug.com/899894")
+    @DisableIf.Build(sdk_is_less_than = VERSION_CODES.M,
+            message = "crbug.com/899894 (for <L), crbug.com/1111816 (for L)")
     public void testRecordTrace() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
         mSettingsActivityTestRule.startSettingsActivity();
