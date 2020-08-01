e1374c4620c1def3f3b141c25e5b21566ca17099
Mugdha Lakhani <nator@chromium.org>
2020-08-01 06:53:14 +0000

    [Prerender] Componentize prerender_util.
    This is used to support NoStatePrefetch for WebLayer.
    
    Bug: 1096088
    Change-Id: I9b2b5e81f58118e7d8167e1b1de19f0b3a18662a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329852
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793882}

--- components/prerender/browser/prerender_util.h
+++ components/prerender/browser/prerender_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
-#define CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
+#ifndef COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
+#define COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
 
 class GURL;
 
@@ -20,4 +20,4 @@ void ReportUnsupportedPrerenderScheme(const GURL& url);
 
 }  // namespace prerender
 
-#endif  // CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
+#endif  // COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
