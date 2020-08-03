3b308f2fd45d49079d40c8b5f32ad89ce760ec07
Stephen Martinis <martiniss@chromium.org>
2020-08-03 16:11:00 +0000

    Remove production freeze
    The freeze is now over!
    
    Bug: 1109980
    Change-Id: If29ad7064c26090ff0e4a2ddc24c697f051f6d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2334807
    Auto-Submit: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    Reviewed-by: Garrett Beaty <gbeaty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794146}

--- infra/config/PRESUBMIT.py
+++ infra/config/PRESUBMIT.py
@@ -9,23 +9,6 @@ for more details on the presubmit API built into depot_tools.
 """
 
 
-# TODO(crbug.com/1109980): Remove this once the production freeze is over, which
-# is expected to be on August 3rd.
-def EnforceProductionFreeze(input_api, output_api):
-  footers = input_api.change.GitFootersFromDescription()
-  if footers.get('Ignore-Cq-Freeze'):
-    return []
-
-  message = """
-  Your change is modifying files which may impact the Chromium CQ. The Chromium
-  CQ is currently in a production freeze. Please get a review from someone in
-  the //infra/OWNERS file (preferably a trooper), and then add the
-  'Ignore-CQ-Freeze' git footer to your CL. See https://crbug.com/1109980 for
-  more details.
-  """
-  return [output_api.PresubmitError(message)]
-
-
 def _CommonChecks(input_api, output_api):
   commands = []
 
@@ -58,7 +41,6 @@ def _CommonChecks(input_api, output_api):
   results.extend(input_api.RunTests(commands))
   results.extend(input_api.canned_checks.CheckChangedLUCIConfigs(
       input_api, output_api))
-  results.extend(EnforceProductionFreeze(input_api, output_api))
 
   return results
 
