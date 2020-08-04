91af65d2228fb593f2c652e0fd6a7bcfaab4b999
Rebekah Potter <rbpotter@chromium.org>
2020-08-04 21:13:04 +0000

    Revert "Composite fullscreen videos with composited descendants."
    This reverts commit 3cea47a949b11fecea6fe6f2f234671b1bd04d93.
    
    Reason for revert: This CL is a likely suspect for breaking
    fullscreen/compositor-touch-hit-rects-fullscrean-video-controls.html test
    from blink_web_tests across multiple platforms.
    
    Original change's description:
    > Composite fullscreen videos with composited descendants.
    >
    > This fixes the referenced performance regression in which some such
    > videos stopped being composited.
    >
    > Bug: 1108972
    >
    > Change-Id: Ia77d33c1c50cf25427d825ae3969a120a8cde640
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2335648
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#794573}
    
    TBR=pdr@chromium.org,chrishtr@chromium.org
    
    Change-Id: I1e1ea71e70e557b867e03f66496bfc97cc182165
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1108972, 1112884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337261
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794717}

--- third_party/blink/renderer/core/paint/compositing/compositing_requirements_updater.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_requirements_updater.cc
@@ -27,7 +27,6 @@
 #include "third_party/blink/renderer/core/paint/compositing/compositing_requirements_updater.h"
 
 #include "base/macros.h"
-#include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.h"
@@ -182,13 +181,6 @@ static CompositingReasons SubtreeReasonsForCompositing(
   if (layer->ShouldIsolateCompositedDescendants()) {
     DCHECK(layer->GetLayoutObject().IsStackingContext());
     subtree_reasons |= CompositingReason::kIsolateCompositedDescendants;
-  }
-
-  if (layer->GetLayoutObject().IsVideo() &&
-      To<HTMLMediaElement>(layer->GetLayoutObject().GetNode())
-          ->IsFullscreen()) {
-    subtree_reasons |=
-        CompositingReason::kFullscreenVideoWithCompositedDescendants;
   }
 
   // A layer with preserve-3d or perspective only needs to be composited if
