f6197a5c018b1d3ab7c0effe6f0ac4849d74a766
Varun Khaneja <vakh@chromium.org>
2020-08-04 19:39:49 +0000

    histograms: Simplify names, update owner, mark a few obsolete
    * SafeBrowsing.Pref.Scout.SetPref.SBER2Pref.* =>
      SafeBrowsing.Pref.Extended.*
    * SafeBrowsing.Pref.SawInterstitial.SBER2Pref =>
      SafeBrowsing.Pref.SawInterstitial
    * Assume ownership of these new histograms (lpz@ is no longer
      working on this code).
    * Mark a lot of suffixes as obsolete.
    
    Fixed: 1089426
    Change-Id: Icc4319e8220adbf7e14a2a8ab8f067449108f5c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333999
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794675}

--- components/safe_browsing/core/common/safe_browsing_prefs.cc
+++ components/safe_browsing/core/common/safe_browsing_prefs.cc
@@ -28,24 +28,20 @@ void RecordExtendedReportingPrefChanged(
 
   switch (location) {
     case safe_browsing::SBER_OPTIN_SITE_CHROME_SETTINGS:
-      UMA_HISTOGRAM_BOOLEAN(
-          "SafeBrowsing.Pref.Scout.SetPref.SBER2Pref.ChromeSettings",
-          pref_value);
+      UMA_HISTOGRAM_BOOLEAN("SafeBrowsing.Pref.Extended.ChromeSettings",
+                            pref_value);
       break;
     case safe_browsing::SBER_OPTIN_SITE_ANDROID_SETTINGS:
-      UMA_HISTOGRAM_BOOLEAN(
-          "SafeBrowsing.Pref.Scout.SetPref.SBER2Pref.AndroidSettings",
-          pref_value);
+      UMA_HISTOGRAM_BOOLEAN("SafeBrowsing.Pref.Extended.AndroidSettings",
+                            pref_value);
       break;
     case safe_browsing::SBER_OPTIN_SITE_DOWNLOAD_FEEDBACK_POPUP:
-      UMA_HISTOGRAM_BOOLEAN(
-          "SafeBrowsing.Pref.Scout.SetPref.SBER2Pref.DownloadPopup",
-          pref_value);
+      UMA_HISTOGRAM_BOOLEAN("SafeBrowsing.Pref.Extended.DownloadPopup",
+                            pref_value);
       break;
     case safe_browsing::SBER_OPTIN_SITE_SECURITY_INTERSTITIAL:
-      UMA_HISTOGRAM_BOOLEAN(
-          "SafeBrowsing.Pref.Scout.SetPref.SBER2Pref.SecurityInterstitial",
-          pref_value);
+      UMA_HISTOGRAM_BOOLEAN("SafeBrowsing.Pref.Extended.SecurityInterstitial",
+                            pref_value);
       break;
     default:
       NOTREACHED();
@@ -199,7 +195,7 @@ void RecordExtendedReportingMetrics(const PrefService& prefs) {
 
   // Track whether this user has ever seen a security interstitial.
   UMA_HISTOGRAM_BOOLEAN(
-      "SafeBrowsing.Pref.SawInterstitial.SBER2Pref",
+      "SafeBrowsing.Pref.SawInterstitial",
       prefs.GetBoolean(prefs::kSafeBrowsingSawInterstitialScoutReporting));
 }
 
