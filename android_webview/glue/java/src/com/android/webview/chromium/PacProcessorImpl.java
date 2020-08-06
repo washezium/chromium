e361b676df50eb6f8a66f4132d9d6211c632b913
Anna Malova <amalova@chromium.org>
2020-08-06 18:12:29 +0000

    Clean up PacProcessorImpl.
    Following the comments from crrev.com/c/2310800.
    
    Bug: 1085115
    Change-Id: I2985b8a8f912009524edbcbb4286687ec6a5c0aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339668
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Anna Malova <amalova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795548}

--- android_webview/glue/java/src/com/android/webview/chromium/PacProcessorImpl.java
+++ android_webview/glue/java/src/com/android/webview/chromium/PacProcessorImpl.java
@@ -9,39 +9,29 @@ import android.webkit.PacProcessor;
 import org.chromium.android_webview.AwPacProcessor;
 import org.chromium.base.JNIUtils;
 import org.chromium.base.library_loader.LibraryLoader;
-import org.chromium.base.library_loader.ProcessInitException;
 
 final class PacProcessorImpl implements PacProcessor {
-    AwPacProcessor mProcessor;
-
     private PacProcessorImpl() {
         JNIUtils.setClassLoader(WebViewChromiumFactoryProvider.class.getClassLoader());
-        try {
-            LibraryLoader.getInstance().ensureInitialized();
-        } catch (ProcessInitException e) {
-            throw new RuntimeException("Error initializing WebView library", e);
-        }
+        LibraryLoader.getInstance().ensureInitialized();
 
         // This will set up Chromium environment to run proxy resolver.
         AwPacProcessor.initializeEnvironment();
-        mProcessor = AwPacProcessor.getInstance();
     }
 
-    private static class LazyHolder {
-        static final PacProcessorImpl sInstance = new PacProcessorImpl();
-    }
+    private static final PacProcessorImpl sInstance = new PacProcessorImpl();
 
     public static PacProcessorImpl getInstance() {
-        return LazyHolder.sInstance;
+        return sInstance;
     }
 
     @Override
     public boolean setProxyScript(String script) {
-        return mProcessor.setProxyScript(script);
+        return AwPacProcessor.getInstance().setProxyScript(script);
     }
 
     @Override
     public String findProxyForUrl(String url) {
-        return mProcessor.makeProxyRequest(url);
+        return AwPacProcessor.getInstance().makeProxyRequest(url);
     }
 }
