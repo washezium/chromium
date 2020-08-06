b3ae06004f3bcf16ef3bed583f91a20d7903f363
Nick Burris <nburris@chromium.org>
2020-08-06 19:31:46 +0000

    [SecurePaymentConfirmation] Create the SecurePaymentConfirmationModel
    This patch adds the model for the secure payment confirmation UI. This
    model holds all of the cross-platform data to be displayed in the UI.
    
    Bug: 1110322
    Change-Id: I4bc041b3712e7795028f37dcd8d883a4aee0d32b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333211
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795590}

---
+++ components/payments/content/secure_payment_confirmation_model.cc
@@ -0,0 +1,13 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/secure_payment_confirmation_model.h"
+
+namespace payments {
+
+SecurePaymentConfirmationModel::SecurePaymentConfirmationModel() = default;
+
+SecurePaymentConfirmationModel::~SecurePaymentConfirmationModel() = default;
+
+}  // namespace payments
