10a28e26f3866876767ddcc59770e7245cc9bd02
Vasilii Sukhanov <vasilii@chromium.org>
2020-08-07 15:20:43 +0000

    Disable RenderWidgetHostViewAuraOverscrollTest.ReverseFlingCancelsOverscroll on Win.
    Tbr: sergeyu@chromium.org
    Bug: 810690
    Change-Id: I657ab563616524df4280855cce7553c692d55a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343265
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795921}

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -3845,7 +3845,7 @@ TEST_F(RenderWidgetHostViewAuraOverscrollTest,
 // Tests that a fling in the opposite direction of the overscroll cancels the
 // overscroll instead of completing it.
 // Flaky on Fuchsia:  http://crbug.com/810690.
-#if defined(OS_FUCHSIA) || defined(OS_LINUX)
+#if defined(OS_FUCHSIA) || defined(OS_LINUX) || defined(OS_WIN)
 #define MAYBE_ReverseFlingCancelsOverscroll \
   DISABLED_ReverseFlingCancelsOverscroll
 #else
