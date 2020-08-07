826ef7da668d36a3a930e63d704c63aa5dda0f04
Vasilii Sukhanov <vasilii@chromium.org>
2020-08-07 11:22:45 +0000

    Disable KioskVirtualKeyboardTest.RestrictFeatures
    Tbr: apotapchuk@chromium.org
    Bug: 1106305
    Change-Id: I031bbc3160cb9dc6510b56ccc80773abb0125a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339750
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795862}

--- chrome/browser/chromeos/login/kiosk_browsertest.cc
+++ chrome/browser/chromeos/login/kiosk_browsertest.cc
@@ -2553,7 +2553,8 @@ class KioskVirtualKeyboardTest : public KioskTest,
   DISALLOW_COPY_AND_ASSIGN(KioskVirtualKeyboardTest);
 };
 
-IN_PROC_BROWSER_TEST_F(KioskVirtualKeyboardTest, RestrictFeatures) {
+// Flaky. crbug.com/1094809
+IN_PROC_BROWSER_TEST_F(KioskVirtualKeyboardTest, DISABLED_RestrictFeatures) {
   set_test_app_id(kTestVirtualKeyboardKioskApp);
   set_test_app_version("0.1");
   set_test_crx_file(test_app_id() + ".crx");
