83c4ec9e34d6e6087cc9aba7673ed7e9817f9214
Alex Gough <ajgo@chromium.org>
2020-08-08 04:29:43 +0000

    Move AudioPreSpawnTarget to utility sandbox delegate.
    No functional changes. Also updates build.gn and deps to reflect move.
    
    Linux hooks serve a different purpose and occur in utility so have not
    been moved.
    
    Bug: 1111421
    Change-Id: I081d31afb421bbe0b906085c8c14e1f9558bab28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340158
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796214}

--- content/browser/utility_sandbox_delegate_win.cc
+++ content/browser/utility_sandbox_delegate_win.cc
@@ -13,10 +13,48 @@
 #include "sandbox/policy/win/sandbox_win.h"
 #include "sandbox/win/src/sandbox_policy.h"
 #include "sandbox/win/src/sandbox_types.h"
-#include "services/audio/audio_sandbox_win.h"
 
 namespace content {
 namespace {
+// Audio:
+//  lockdown_level_(sandbox::USER_LOCKDOWN),
+//  initial_level_(sandbox::USER_RESTRICTED_SAME_ACCESS),
+//
+//  job_level_(sandbox::JOB_LOCKDOWN),
+//
+//  integrity_level_(sandbox::INTEGRITY_LEVEL_LOW),
+//  delayed_integrity_level_(sandbox::INTEGRITY_LEVEL_UNTRUSTED),
+bool AudioPreSpawnTarget(sandbox::TargetPolicy* policy) {
+  // Audio process privilege requirements:
+  //  - Lockdown level of USER_NON_ADMIN
+  //  - Delayed integrity level of INTEGRITY_LEVEL_LOW
+  //
+  // For audio streams to create shared memory regions, lockdown level must be
+  // at least USER_LIMITED and delayed integrity level INTEGRITY_LEVEL_LOW,
+  // otherwise CreateFileMapping() will fail with error code ERROR_ACCESS_DENIED
+  // (0x5).
+  //
+  // For audio input streams to use ISimpleAudioVolume interface, lockdown
+  // level must be set to USER_NON_ADMIN, otherwise
+  // WASAPIAudioInputStream::Open() will fail with error code E_ACCESSDENIED
+  // (0x80070005) when trying to get a reference to ISimpleAudioVolume
+  // interface. See
+  // https://cs.chromium.org/chromium/src/media/audio/win/audio_low_latency_input_win.cc
+  // Use USER_RESTRICTED_NON_ADMIN over USER_NON_ADMIN to prevent failures when
+  // AppLocker and similar application whitelisting solutions are in place.
+  policy->SetTokenLevel(sandbox::USER_RESTRICTED_SAME_ACCESS,
+                        sandbox::USER_RESTRICTED_NON_ADMIN);
+  policy->SetDelayedIntegrityLevel(sandbox::INTEGRITY_LEVEL_LOW);
+
+  // Custom default policy allowing audio drivers to read device properties
+  // (https://crbug.com/883326).
+  policy->SetIntegrityLevel(sandbox::INTEGRITY_LEVEL_LOW);
+  policy->SetLockdownDefaultDacl();
+  policy->SetAlternateDesktop(true);
+
+  return true;
+}
+
 // Right now, this policy is essentially unsandboxed, but with default process
 // mitigations applied.
 // TODO(https://crbug.com/841001) This will be tighted up in future releases.
@@ -73,7 +111,7 @@ bool UtilitySandboxedProcessLauncherDelegate::PreSpawnTarget(
     return NetworkPreSpawnTarget(policy, cmd_line_);
 
   if (sandbox_type_ == sandbox::policy::SandboxType::kAudio)
-    return audio::AudioPreSpawnTarget(policy);
+    return AudioPreSpawnTarget(policy);
 
   if (sandbox_type_ == sandbox::policy::SandboxType::kProxyResolver) {
     sandbox::MitigationFlags flags = policy->GetDelayedProcessMitigations();
