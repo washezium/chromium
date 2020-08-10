8f92c37d702e83d9142095deb9dd5b769877f748
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2020-08-10 18:17:44 +0000

    Fix Role::kButton name invalidation when descendant text is modified
    This CL addresses a bug where Role::kButton, and possibly other roles
    that derive their name from descendant contents, were not firing
    Text Changed when a descendant Node.TEXT_NODE modifies its text |data|.
    
    ax_object_cache_impl.cc :
    - When firing a text changed event, also fire a text changed event for
      the first ancestor that derives its name from descendant contents.
      This ensures the nodes are invalidated, send appropriate text changed
      events, and the changes are serialized to the browser process AXTree.
    
    browser_accessibility_manager_win.cc :
    - FireUiaTextContainerEvent made it possible to fire text events more
      than once for a given text provider so it has been removed.
    - Instead, populate a set |text_changed_events_| which contains the
      text providers via |GetUiaTextPatternProvider| and fire the UIA text
      events in BrowserAccessibilityManagerWin::FinalizeAccessibilityEvents
    - |text_selection_changed_events_| could also potentially end up with
      the same double-event problem, so it uses GetUiaTextPatternProvider
      now as well.
    - Removing usage of std::unordered_set based on the guidance in
      base/containers/README.md. In general these sets shouldn't grow large
      enough to benefit from being unordered.
    
    AX-Relnotes: Fix bug where after updating the content of complex button might not notify ATs, causing the wrong accessible name to be announced.
    Bug: 1105262
    Change-Id: I02dc7798686b09f780c90af52aeb2e36fe8fe048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296564
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796464}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -1720,7 +1720,7 @@ bool AXObject::CanSetFocusAttribute() const {
   // TODO(accessibility) Focusable: scrollable with the keyboard.
   // Keyboard-focusable scroll containers feature:
   // https://www.chromestatus.com/feature/5231964663578624
-  // When adding here, remove similar check from ::NameFromContents().
+  // When adding here, remove similar check from ::SupportsNameFromContents().
   // if (RuntimeEnabledFeatures::KeyboardFocusableScrollersEnabled() &&
   //     IsUserScrollable()) {
   //   return true;
@@ -3942,7 +3942,7 @@ bool AXObject::NameFromSelectedOption(bool recursive) const {
   }
 }
 
-bool AXObject::NameFromContents(bool recursive) const {
+bool AXObject::SupportsNameFromContents(bool recursive) const {
   // ARIA 1.1, section 5.2.7.5.
   bool result = false;
 
