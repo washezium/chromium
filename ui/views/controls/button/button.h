a1aebd9691fe555e585ea1a243e7c0b978326cf7
Peter Kasting <pkasting@chromium.org>
2020-08-11 00:22:08 +0000

    Reland r786979, part 4: remove ButtonObserver.
    The original CL was landed in one piece and reverted due to apparent
    perf regressions.  Reland in sections to try and determine more
    precisely whether there is any perf impact.
    
    This CL removes ButtonObserver entirely, which should be unused.
    
    If this causes perf regressions... something about cache alignment??
    
    Bug: none
    Change-Id: If9c0f40d50c9122d5830067eab0b2e2d63d00b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346535
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796638}

--- ui/views/controls/button/button.h
+++ ui/views/controls/button/button.h
@@ -27,7 +27,6 @@ class ButtonTestApi;
 
 class Button;
 class ButtonController;
-class ButtonObserver;
 class Event;
 
 // An interface implemented by an object to let it know that a button was
@@ -178,8 +177,6 @@ class VIEWS_EXPORT Button : public InkDropHostView,
   // Highlights the ink drop for the button.
   void SetHighlighted(bool bubble_visible);
 
-  void AddButtonObserver(ButtonObserver* observer);
-  void RemoveButtonObserver(ButtonObserver* observer);
   PropertyChangedSubscription AddStateChangedCallback(
       PropertyChangedCallback callback);
 
@@ -356,8 +353,6 @@ class VIEWS_EXPORT Button : public InkDropHostView,
   PropertyChangedSubscription enabled_changed_subscription_{
       AddEnabledChangedCallback(base::BindRepeating(&Button::OnEnabledChanged,
                                                     base::Unretained(this)))};
-
-  base::ObserverList<ButtonObserver> button_observers_;
 
   DISALLOW_COPY_AND_ASSIGN(Button);
 };
