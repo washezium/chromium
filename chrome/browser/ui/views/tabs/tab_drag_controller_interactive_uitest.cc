f291260f1567c05207794ff81daf4e5b268e0918
Taylor Bergquist <tbergquist@chromium.org>
2020-08-17 20:00:46 +0000

    Revert "Reland "Tab strip only occupies the space needed for its tabs.""
    This reverts commit b25056de332bb7de373f883e116b6b9add3ee413.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1116917
    
    Original change's description:
    > Reland "Tab strip only occupies the space needed for its tabs."
    > 
    > This is a reland of b6c0ca2afd6144b0a88f9650029606f01426f144
    > 
    > The only change is a line in tab_scrubber_browsertest.cc that ensures
    > that any invalid layout are laid out after modifying the tabstrip.
    > 
    > Original change's description:
    > > Tab strip only occupies the space needed for its tabs.
    > >
    > > Bug: 1093972
    > > Change-Id: I0ca6cc1a7dad9240b85745fefb39ac4f52eb9355
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300993
    > > Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    > > Reviewed-by: Connie Wan <connily@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#793270}
    > 
    > Bug: 1093972
    > Change-Id: I4ded68fde336b14b38ad862cd63118ebc9fc7a72
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336296
    > Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    > Reviewed-by: Connie Wan <connily@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#797853}
    
    TBR=pbos@chromium.org,tbergquist@chromium.org,connily@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1093972
    Change-Id: I3de1d647a4fbba593ce3ef52f59127b6495e5ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358591
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798790}

--- chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
@@ -254,10 +254,6 @@ void TabDragControllerTest::AddTabsAndResetBrowser(Browser* browser,
   }
   browser->window()->Show();
   StopAnimating(GetTabStripForBrowser(browser));
-  // Perform any pending layouts happen so the tabstrip is in a steady state.
-  BrowserView::GetBrowserViewForBrowser(browser)
-      ->GetWidget()
-      ->LayoutRootViewIfNecessary();
   ResetIDs(browser->tab_strip_model(), 0);
 }
 
