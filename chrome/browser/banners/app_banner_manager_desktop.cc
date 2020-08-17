3b08f5099a526ad32033184cbf8e709de93776c3
Mike Jackson <mjackson@microsoft.com>
2020-08-17 02:12:38 +0000

    dpwas: Update AppBannerManagerDesktop to get winning display mode
    Update AppBannerManagerDesktop::OnWebAppInstalled to use the
    winning display mode when display_override is set.
    
    To test the change use: chrome --enable-features=WebAppManifestDisplayOverride
    Example websites:
     https://mwjacksonmsft.github.io/pwa/display-override-custom/index.html
     (display == browser, display_override == [standard])
    
    
    Explainer: https://github.com/WICG/display-override/blob/master/explainer.m
    Design document: https://docs.google.com/document/d/1hEmbGVHMN38q1YTaaGccQ-Y5CHr7xIURYPRWXTuvZLo/edit?usp=sharing
    I2P: https://groups.google.com/a/chromium.org/d/topic/blink-dev/WvIeZT8uSzw/discussion
    
    Bug: 1109520
    Change-Id: If0b589f0a19367f01501e2eb52aec16f4f2645cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354665
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#798519}

--- chrome/browser/banners/app_banner_manager_desktop.cc
+++ chrome/browser/banners/app_banner_manager_desktop.cc
@@ -223,7 +223,7 @@ void AppBannerManagerDesktop::OnWebAppInstalled(
   if (app_id.has_value() && *app_id == installed_app_id &&
       registrar().GetAppUserDisplayMode(*app_id) ==
           blink::mojom::DisplayMode::kStandalone) {
-    OnInstall(registrar().GetAppDisplayMode(*app_id));
+    OnInstall(registrar().GetEffectiveDisplayModeFromManifest(*app_id));
     SetInstallableWebAppCheckResult(InstallableWebAppCheckResult::kNo);
   }
 }
