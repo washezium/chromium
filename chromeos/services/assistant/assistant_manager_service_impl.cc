c1b54aee013f9071747681f3cc486714dad99563
Xiaohui Chen <xiaohuic@google.com>
2020-08-17 21:34:45 +0000

    assistant: add a few DVLOG for future debugging
    Bug: None
    Change-Id: I063f5d074dac5d7c02d50cdc3e8975f30f80a214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2357422
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798837}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -466,6 +466,7 @@ void AssistantManagerServiceImpl::UpdateInternalMediaPlayerStatus(
 
 void AssistantManagerServiceImpl::StartVoiceInteraction() {
   DCHECK(assistant_manager_);
+  DVLOG(1) << __FUNCTION__;
 
   platform_api_->SetMicState(true);
   assistant_manager_->StartAssistantInteraction();
@@ -473,6 +474,7 @@ void AssistantManagerServiceImpl::StartVoiceInteraction() {
 
 void AssistantManagerServiceImpl::StopActiveInteraction(
     bool cancel_conversation) {
+  DVLOG(1) << __FUNCTION__;
   platform_api_->SetMicState(false);
 
   if (!assistant_manager_internal_) {
@@ -520,6 +522,7 @@ void AssistantManagerServiceImpl::StartTextInteraction(
     const std::string& query,
     AssistantQuerySource source,
     bool allow_tts) {
+  DVLOG(1) << __FUNCTION__;
   assistant_client::VoicelessOptions options;
   options.is_user_initiated = true;
   options.enable_on_device_assistant_for_voiceless =
