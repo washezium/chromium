4c4003f92887d2cd3ef6a337e40f583259b0b14d
anthonyvd <anthonyvd@chromium.org>
2020-08-17 16:23:52 +0000

    Revert "Adds Desugar's jdk_libs_configuration to the build"
    This reverts commit 8444271fc57dc6cf78bec0356882e111460d16fa.
    
    Reason for revert: Causing build failures, see https://ci.chromium.org/p/chromium/builders/ci/android-archive-rel/14156?
    
    Original change's description:
    > Adds Desugar's jdk_libs_configuration to the build
    > 
    > This contains information required for new Java 8 desugaring features.
    > 
    > Bug: 1056751
    > Change-Id: I87e15cd74f6a4a714fae748ed61172c000eda67c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354518
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798651}
    
    TBR=yfriedman@chromium.org,dewittj@chromium.org,wnwen@chromium.org,agrieve@chromium.org,echristo@google.com
    
    Change-Id: I21ea1a52845d3134339aa426e5699c452ac0e538
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1056751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358956
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798661}

--- third_party/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
@@ -35,8 +35,8 @@ class ChromiumDepGraph {
             licenseUrl: "https://raw.githubusercontent.com/google/desugar_jdk_libs/master/LICENSE",
             generateTarget: false),
         'com_android_tools_desugar_jdk_libs_configuration': new PropertyOverride(
-            licensePath: "licenses/desugar_jdk_libs_configuration.txt",
-            licenseName: "BSD 3-Clause"),
+            // Configuration stored in //third_party/r8.
+            exclude: true),
         'backport_util_concurrent_backport_util_concurrent': new PropertyOverride(
             licensePath: "licenses/CC01.0.txt",
             licenseName: "CC0 1.0"),
