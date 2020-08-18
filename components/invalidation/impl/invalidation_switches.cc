a553d87c70a1243eaced72604cc3f254d5a7edbf
Marc Treib <treib@chromium.org>
2020-08-18 12:37:50 +0000

    Remove unused feature TiclInvalidationsStartInvalidatorOnActiveHandler
    Bug: none
    Change-Id: I2499bc3c0c78e19bad3c67341d90dae74d2c0cb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360269
    Commit-Queue: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#799069}

--- components/invalidation/impl/invalidation_switches.cc
+++ components/invalidation/impl/invalidation_switches.cc
@@ -22,17 +22,9 @@ const base::Feature kFCMInvalidationsStartOnceActiveAccountAvailable = {
     "FCMInvalidationsStartOnceActiveAccountAvailable",
     base::FEATURE_ENABLED_BY_DEFAULT};
 
-extern const base::Feature kFCMInvalidationsForSyncDontCheckVersion;
 const base::Feature kFCMInvalidationsForSyncDontCheckVersion = {
     "FCMInvalidationsForSyncDontCheckVersion",
     base::FEATURE_ENABLED_BY_DEFAULT};
-
-// TODO(melandory): Once FCM invalidations are launched, this feature toggle
-// should be removed.
-// TODO(crbug.com/964296): Re-enable when bug is resolved.
-const base::Feature kTiclInvalidationsStartInvalidatorOnActiveHandler = {
-    "TiclInvalidationsStartInvalidatorOnActiveHandler",
-    base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kSyncInstanceIDTokenTTL {
   "SyncInstanceIDTokenTTL",
