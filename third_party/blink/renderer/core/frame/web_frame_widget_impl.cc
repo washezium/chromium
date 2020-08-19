b68142a102717c16c53b2a4fd95d044ec531127b
Ionel Popescu <iopopesc@microsoft.com>
2020-08-19 02:53:31 +0000

    Ensure that WebFrameWidgetImpl hands char events off to an open popup.
    This CL addresses an issue where the users where not able to enter data
    into the text fields of the color picker when the elements are inside
    a cross-domain iframe.
    
    For the top-level page and for same-domain iframes, when a char event
    is received while a popup is open, the event is routed from
    RenderWidgetInputHandler to WebViewFrameWidget shim, which hands it to
    the WebView. WebViewImpl::HandleCharEvent has code to check for an open
    popup and the event is sent to the popup if one exists.
    
    For the cross-domain iframe case, the RenderWidgetInputHandler hands
    the event to WebFrameWidgetImpl, but WebFrameWidgetImpl::HandleCharEvent
    doesn't check for an open popup, so the event goes to the page
    containing the popup.
    
    This CL adds the same popup check to WebFrameWidgetImpl as the one that
    WebViewImpl::HandleCharEvent already has.
    
    Bug: 1114052
    Change-Id: I33f2b10b4ae5aa538ebb0d22c1d55f5d97019216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364008
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#799458}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -976,6 +976,12 @@ WebInputEventResult WebFrameWidgetImpl::HandleCharEvent(
   bool suppress = suppress_next_keypress_event_;
   suppress_next_keypress_event_ = false;
 
+  // If there is a popup open, it should be the one processing the event,
+  // not the page.
+  scoped_refptr<WebPagePopupImpl> page_popup = View()->GetPagePopup();
+  if (page_popup)
+    return page_popup->HandleKeyEvent(event);
+
   LocalFrame* frame = To<LocalFrame>(FocusedCoreFrame());
   if (!frame) {
     return suppress ? WebInputEventResult::kHandledSuppressed
