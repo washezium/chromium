0a3533b5e61b4066fb4324c92b3e4c672f1e3a1b
Kent Tamura <tkent@chromium.org>
2020-08-19 02:16:40 +0000

    Move layout_ng_button.* out from layout/ng/flex/
    LayoutNGButton inherits from LayoutNGFlexibleBox.  However it's an
    implementation detail, and we might change the base class in the future.
    
    This CL has no behavior changes.
    
    Bug: 1040826
    Change-Id: I71aabd4b3bfdc9d1b6612c97aeb1fd3bff08a15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362347
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799434}

--- third_party/blink/renderer/core/layout/ng/layout_ng_button.h
+++ third_party/blink/renderer/core/layout/ng/layout_ng_button.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_FLEX_LAYOUT_NG_BUTTON_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_FLEX_LAYOUT_NG_BUTTON_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_BUTTON_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_BUTTON_H_
 
 #include "third_party/blink/renderer/core/layout/ng/flex/layout_ng_flexible_box.h"
 
@@ -34,4 +34,4 @@ class LayoutNGButton final : public LayoutNGFlexibleBox {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_FLEX_LAYOUT_NG_BUTTON_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_BUTTON_H_
