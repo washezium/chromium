6b0d8b639abe58259ffb29954105d54ab1e796d9
Hajime Hoshi <hajimehoshi@chromium.org>
2020-08-19 05:10:26 +0000

    Disable DeleteSpeculativeRFHPendingCommitOfPendingEntryOnInterrupted2 on macOS
    This test is flaky also on macOS.
    
    TBR=clamy@chromium.org
    
    Bug: 796135
    Change-Id: I8275f5c3267ed072e3eed79e81c93067ecc94c50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362052
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799502}

--- content/browser/frame_host/render_frame_host_manager_browsertest.cc
+++ content/browser/frame_host/render_frame_host_manager_browsertest.cc
@@ -1942,7 +1942,8 @@ IN_PROC_BROWSER_TEST_P(
   EXPECT_FALSE(speculative_rfh);
 }
 
-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
+    defined(OS_MAC)
 #define MAYBE_DeleteSpeculativeRFHPendingCommitOfPendingEntryOnInterrupted2 \
   DISABLED_DeleteSpeculativeRFHPendingCommitOfPendingEntryOnInterrupted2
 #else
