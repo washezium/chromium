ef0ab61f93eafa8dec610ffe2df3127ff38c995b
Yuri Wiitala <miu@chromium.org>
2020-08-19 02:13:17 +0000

    Revert "[Sheriff] Disable the test"
    This reverts commit af48c8e640b31cfcd75fe9c26e85be4cd7ec4a5b.
    
    Reason for revert: Test works now.
    
    Original change's description:
    > [Sheriff] Disable the test
    > 
    > Disable the failing test on Linux.
    > 
    > Bug=1108205
    > TBR=miu@chromium.org
    > 
    > Change-Id: Ie388ca75a2fadce937d613994847217e7f9579fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310271
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Commit-Queue: Hayato Ito <hayato@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790745}
    
    TBR=miu@chromium.org,hayato@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I4c43284f7d4a0fec1cd4c05fc375f1f4d406526b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2361023
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799428}

--- content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
+++ content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
@@ -400,15 +400,8 @@ INSTANTIATE_TEST_SUITE_P(
 // whether the browser is running with software compositing or GPU-accelerated
 // compositing, whether the WebContents is visible/hidden or occluded/unoccluded
 // and whether the main document contains a cross-site iframe.
-
-// Fails on Linux only. http://crbug.com/1108205
-#if defined(OS_LINUX)
-#define MAYBE_CapturesContentChanges DISABLED_CapturesContentChanges
-#else
-#define MAYBE_CapturesContentChanges CapturesContentChanges
-#endif
 IN_PROC_BROWSER_TEST_P(WebContentsVideoCaptureDeviceBrowserTestP,
-                       MAYBE_CapturesContentChanges) {
+                       CapturesContentChanges) {
   SCOPED_TRACE(testing::Message()
                << "Test parameters: "
                << (IsSoftwareCompositingTest() ? "Software Compositing"
