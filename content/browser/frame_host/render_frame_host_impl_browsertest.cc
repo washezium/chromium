0ad9a3ec4ada1f13898f7659eb87e13bee2c9da4
Christian Dullweber <dullweber@chromium.org>
2020-08-19 16:55:47 +0000

    Disable CheckIsCurrentBeforeAndAfterUnload test
    RenderFrameHostImplBrowserTest.CheckIsCurrentBeforeAndAfterUnload is
    flaking frequently.
    
    Tbr: acolwell@chromium.org
    Bug: 1114149
    Change-Id: If6f3066dc64196a39e9da9da62750f019c453a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362801
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799679}

--- content/browser/frame_host/render_frame_host_impl_browsertest.cc
+++ content/browser/frame_host/render_frame_host_impl_browsertest.cc
@@ -3609,8 +3609,9 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest, WebUiReloadAfterCrash) {
 // Start with A(B), navigate A to C. By emulating a slow unload handler B, check
 // the status of IsCurrent for subframes of A i.e., B before and after
 // navigating to C.
+// Test is flaky: https://crbug.com/1114149.
 IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
-                       CheckIsCurrentBeforeAndAfterUnload) {
+                       DISABLED_CheckIsCurrentBeforeAndAfterUnload) {
   IsolateAllSitesForTesting(base::CommandLine::ForCurrentProcess());
   GURL url_ab(embedded_test_server()->GetURL(
       "a.com", "/cross_site_iframe_factory.html?a(b)"));
