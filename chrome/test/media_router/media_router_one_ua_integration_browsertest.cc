6b4136e340388619418877683f0aeb1e7f499821
Friedrich Horschig <fhorschig@chromium.org>
2020-08-21 10:25:42 +0000

    [CrOS][Flaky] Disable flaky MediaRouterIntegration* tests
    After this CL, CrOS tests will only execute 2 more tests from this test
    suite but the flakiness has reached a frequency of 1 failure per run
    (not the same test though).
    The failure reason is not easy to track down but the error log is pretty
    much always show the same DCHECK being hit:
    Check failed: casting_session_count_ > 0 (0 vs. 0)
         #0 0x561ba221ecab in __interceptor_backtrace /b/s/w/ir/cache/builder/src/third_party/llvm/compiler-rt/lib/asan/../sanitizer_common/sanitizer_common_interceptors.inc:4176
         #1 0x561ba221ecab in ?? ??:0
         #2 0x561bb683aea9 in base::debug::CollectStackTrace(void**, unsigned long) ./../../base/debug/stack_trace_posix.cc:840
         #3 0x561bb683aea9 in ?? ??:0
         #4 0x561bb656d143 in StackTrace ./../../base/debug/stack_trace.cc:206
         #5 0x561bb656d143 in StackTrace ./../../base/debug/stack_trace.cc:203
         #6 0x561bb656d143 in ?? ??:0
         #7 0x561bb65cd766 in logging::LogMessage::~LogMessage() ./../../base/logging.cc:543
         #8 0x561bb65cd766 in ?? ??:0
         #9 0x561bb65cee7e in logging::LogMessage::~LogMessage() ./../../base/logging.cc:537
         #10 0x561bb65cee7e in ?? ??:0
         #11 0x561bc20145a9 in ash::ProjectingObserver::OnCastingSessionStartedOrStopped(bool) ./../../ash/display/projecting_observer.cc:50
         #12 0x561bc20145a9 in ?? ??:0
         #13 0x561bc1fe0c33 in ash::Shell::OnCastingSessionStartedOrStopped(bool) ./../../ash/shell.cc:404
         #14 0x561bc1fe0c33 in ?? ??:0
         #15 0x561bb6cc6e86 in MediaCaptureDevicesDispatcher::UpdateMediaRequestStateOnUIThread(int, int, int, GURL const&, blink::mojom::MediaStreamType, content::MediaRequestState) media_capture_devices_dispatcher.cc:?
         #16 0x561bb6cc6e86 in ?? ??:0
         #17 0x561bb6cc9822 in Invoke<void (MediaCaptureDevicesDispatcher::*)(int, int, int, const GURL &, blink::mojom::MediaStreamType, content::MediaRequestState), MediaCaptureDevicesDispatcher *, int, int, int, GURL, blink::mojom::MediaStreamType, content::MediaRequestState> ./../../base/bind_internal.h:498
         #18 0x561bb6cc9822 in MakeItSo<void (MediaCaptureDevicesDispatcher::*)(int, int, int, const GURL &, blink::mojom::MediaStreamType, content::MediaRequestState), MediaCaptureDevicesDispatcher *, int, int, int, GURL, blink::mojom::MediaStreamType, content::MediaRequestState> ./../../base/bind_internal.h:637
         #19 0x561bb6cc9822 in RunImpl<void (MediaCaptureDevicesDispatcher::*)(int, int, int, const GURL &, blink::mojom::MediaStreamType, content::MediaRequestState), std::__1::tuple<base::internal::UnretainedWrapper<MediaCaptureDevicesDispatcher>, int, int, int, GURL, blink::mojom::MediaStreamType, content::MediaRequestState>, 0, 1, 2, 3, 4, 5, 6> ./../../base/bind_internal.h:710
         #20 0x561bb6cc9822 in RunOnce ./../../base/bind_internal.h:679
         #21 0x561bb6cc9822 in ?? ??:0
         #22 0x561bb6702dd1 in Run ./../../base/callback.h:99
    
    TBR: mfoltz@chromium.org
    Bug: 822231
    Change-Id: I1780e4f0be20361bded9d38a070e6dfacdfdd45e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367676
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800528}

--- chrome/test/media_router/media_router_one_ua_integration_browsertest.cc
+++ chrome/test/media_router/media_router_one_ua_integration_browsertest.cc
@@ -67,14 +67,25 @@ IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUABrowserTest,
   ExecuteJavaScriptAPI(web_contents, kInitiateCloseFromReceiverPageScript);
 }
 
+// TODO(https://crbug.com/822231): Flaky in Chromium waterfall.
+#if defined(OS_CHROMEOS)
+#define MAYBE_Fail_SendMessage DISABLED_Fail_SendMessage
+#else
+#define MAYBE_Fail_SendMessage Fail_SendMessage
+#endif
 IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUABrowserTest,
-                       Fail_SendMessage) {
+                       MAYBE_Fail_SendMessage) {
   RunFailToSendMessageTest();
 }
-#if (defined(OS_LINUX) || defined(OS_CHROMEOS)) &&              \
-    (BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
-     BUILDFLAG(CFI_ENFORCEMENT_TRAP) || BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC))
+#undef MAYBE_Fail_SendMessage
+
+#if defined(OS_CHROMEOS) ||                                      \
+    (defined(OS_LINUX) &&                                        \
+     (BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
+      BUILDFLAG(CFI_ENFORCEMENT_TRAP) ||                         \
+      BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC)))
 // https://crbug.com/966827. Flaky on Linux CFI.
+// TODO(https://crbug.com/822231): Flaky in Chromium OS waterfall.
 #define MAYBE_ReconnectSession DISABLED_ReconnectSession
 #else
 #define MAYBE_ReconnectSession ReconnectSession
@@ -95,6 +106,7 @@ IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUABrowserTest,
                        MAYBE_ReconnectSessionSameTab) {
   RunReconnectSessionSameTabTest();
 }
+#undef MAYBE_ReconnectSessionSameTab
 
 class MediaRouterIntegrationOneUANoReceiverBrowserTest
     : public MediaRouterIntegrationOneUABrowserTest {
@@ -105,24 +117,51 @@ class MediaRouterIntegrationOneUANoReceiverBrowserTest
   }
 };
 
+// TODO(https://crbug.com/822231): Flaky in Chromium waterfall.
+#if defined(OS_CHROMEOS)
+#define MAYBE_Basic DISABLED_Basic
+#else
+#define MAYBE_Basic Basic
+#endif
 IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUANoReceiverBrowserTest,
-                       Basic) {
+                       MAYBE_Basic) {
   RunBasicTest();
 }
+#undef MAYBE_Basic
 
+// TODO(https://crbug.com/822231): Flaky in Chromium waterfall.
+#if defined(OS_CHROMEOS)
+#define MAYBE_Fail_SendMessage DISABLED_Fail_SendMessage
+#else
+#define MAYBE_Fail_SendMessage Fail_SendMessage
+#endif
 IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUANoReceiverBrowserTest,
-                       Fail_SendMessage) {
+                       MAYBE_Fail_SendMessage) {
   RunFailToSendMessageTest();
 }
+#undef MAYBE_Fail_SendMessage
 
+// TODO(https://crbug.com/822231): Flaky in Chromium waterfall.
+#if defined(OS_CHROMEOS)
+#define MAYBE_ReconnectSession DISABLED_ReconnectSession
+#else
+#define MAYBE_ReconnectSession ReconnectSession
+#endif
 IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUANoReceiverBrowserTest,
-                       ReconnectSession) {
+                       MAYBE_ReconnectSession) {
   RunReconnectSessionTest();
 }
 
+// TODO(https://crbug.com/822231): Flaky in Chromium waterfall.
+#if defined(OS_CHROMEOS)
+#define MAYBE_ReconnectSessionSameTab DISABLED_ReconnectSessionSameTab
+#else
+#define MAYBE_ReconnectSessionSameTab ReconnectSessionSameTab
+#endif
 IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationOneUANoReceiverBrowserTest,
-                       ReconnectSessionSameTab) {
+                       MAYBE_ReconnectSessionSameTab) {
   RunReconnectSessionSameTabTest();
 }
+#undef MAYBE_ReconnectSessionSameTab
 
 }  // namespace media_router
