636a80ac409635e6cde51efbc9a1e12ed4665e0a
Leonard Grey <lgrey@chromium.org>
2020-08-21 17:00:32 +0000

    Commander views layer
    This is responsible for:
    - Mediating between the controller and WebUI layers.
    - Managing the widget the WebUI interface lives in.
    - Holding the web view the WebUI interface lives in. This is necessary
      because creating a renderer on demand caused unacceptable latency in
      the commander prototype.
    
    Reviewer note: Please note that there are 5 separate paths to close
    - Dismissed by controller via ViewModel
    - Dismissed by browser via Hide
    - Dismissed by WebUI via OnDismiss
    - Window closed (possibly from parent window closing)
    - Object destroyed
    I'm pretty sure they're all safe, but this part definitely needs a
    second pair of eyes.
    
    Bug: 1014639
    Change-Id: I422754923a0be3ce3e2b7b6a500add76f7e5ecd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364225
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800609}

---
+++ chrome/browser/ui/views/commander_frontend_views.h
@@ -0,0 +1,90 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_COMMANDER_FRONTEND_VIEWS_H_
+#define CHROME_BROWSER_UI_VIEWS_COMMANDER_FRONTEND_VIEWS_H_
+
+#include "chrome/browser/ui/commander/commander_frontend.h"
+
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/profiles/profile_manager.h"
+
+class CommanderWebView;
+
+namespace views {
+class WidgetDelegate;
+class Widget;
+}  // namespace views
+
+namespace commander {
+class CommanderBackend;
+struct CommanderViewModel;
+}  // namespace commander
+
+// Views implementation of commander::CommanderFrontend. The actual UI is WebUI;
+// this class is responsible for setting up the infrastructure to host the
+// WebUI in its own widget and mediating between the WebUI implementation and
+// the controller.
+class CommanderFrontendViews : public commander::CommanderFrontend {
+ public:
+  explicit CommanderFrontendViews(commander::CommanderBackend* backend);
+  ~CommanderFrontendViews() override;
+
+  // commander::CommanderFrontend overrides
+  void Show(Browser* browser) override;
+  void Hide() override;
+
+  // TODO(lgrey): When the WebUI layer is added, these declarations should
+  // be moved to the CommanderHandler::Delegate interface.
+  // Called when the text is changed in the WebUI interface.
+  void OnTextChanged(const base::string16& text);
+  // Called when an option is selected (clicked or enter pressed) in the WebUI
+  // interface.
+  void OnOptionSelected(size_t option_index, int result_set_id);
+  // Called when the WebUI interface wants to dismiss the UI.
+  void OnDismiss();
+  // Called when the WebUI interface's content height has changed.
+  void OnHeightChanged(int new_height);
+
+ private:
+  // Receives view model updates from |backend_|.
+  void OnViewModelUpdated(commander::CommanderViewModel view_model);
+  // Receives system profile from CreateProfileAsync()
+  void OnSystemProfileAvailable(Profile* profile, Profile::CreateStatus status);
+
+  // Creates a web_view_ to host the WebUI interface for |profile|. Should only
+  // be called once when the system profile becomes available.
+  void CreateWebView(Profile* profile);
+
+  // Called by widget delegate when widget closes.
+  void OnWindowClosing();
+
+  bool is_showing() { return widget_ != nullptr; }
+  bool is_web_view_created() {
+    return is_showing() || web_view_.get() != nullptr;
+  }
+
+  // The controller. Must outlive this object.
+  commander::CommanderBackend* backend_;
+  // True if Show() was called before the system profile is available.
+  // If this is set, CreateWebView() will call Show().
+  bool show_requested_ = false;
+  // The widget that contains |web_view_ptr_|
+  views::Widget* widget_ = nullptr;
+  // |widget_|'s delegate
+  std::unique_ptr<views::WidgetDelegate> widget_delegate_;
+  // The web_view_ that hosts the WebUI instance. Populated when the view
+  // is showing and null otherwise.
+  CommanderWebView* web_view_ptr_ = nullptr;
+  // |web_view_ptr_| is held here when the widget is *not* showing.
+  std::unique_ptr<CommanderWebView> web_view_;
+  // The browser |widget_| is attached to.
+  Browser* browser_;
+
+  base::WeakPtrFactory<CommanderFrontendViews> weak_ptr_factory_{this};
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_COMMANDER_FRONTEND_VIEWS_H_
