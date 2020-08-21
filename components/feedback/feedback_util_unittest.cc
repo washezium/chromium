9efa79073b15ad156c4a795cf3f35d4cc677e54a
Jenny Zhang <jennyz@chromium.org>
2020-08-21 00:50:06 +0000

    Refactor: Move ReadEndOfFile function into feedback_util.
    The function will be used by Lacros system log source.
    
    Bug:1109387
    TEST: Send feedback reports and verify nothing is changed.
    
    Change-Id: Id1bf276a10d1389b3ea1872d03a561c97490eea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368153
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800393}

--- components/feedback/feedback_util_unittest.cc
+++ components/feedback/feedback_util_unittest.cc
@@ -1,8 +1,8 @@
-// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/system_logs/debug_daemon_log_source.h"
+#include "components/feedback/feedback_util.h"
 
 #include <string>
 
@@ -12,27 +12,29 @@
 #include "base/rand_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace system_logs {
+namespace feedback_util {
 
-class DebugDaemonLogSourceTest : public ::testing::Test {
+// Note: This file is excluded from win build.
+// See https://crbug.com/1119560.
+class FeedbackUtilTest : public ::testing::Test {
  public:
   void SetUp() override { ASSERT_TRUE(temp_dir_.CreateUniqueTempDir()); }
 
   base::ScopedTempDir temp_dir_;
 };
 
-TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileEmpty) {
+TEST_F(FeedbackUtilTest, ReadEndOfFileEmpty) {
   std::string read_data("should be erased");
 
   base::FilePath file_path = temp_dir_.GetPath().Append("test_empty.txt");
 
   WriteFile(file_path, "", 0);
 
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 10));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 10, &read_data));
   EXPECT_EQ(0u, read_data.length());
 }
 
-TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileSmall) {
+TEST_F(FeedbackUtilTest, ReadEndOfFileSmall) {
   const char kTestData[] = "0123456789";  // Length of 10
   std::string read_data;
 
@@ -41,27 +43,27 @@ TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileSmall) {
   WriteFile(file_path, kTestData, strlen(kTestData));
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 15));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 15, &read_data));
   EXPECT_EQ(kTestData, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 10));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 10, &read_data));
   EXPECT_EQ(kTestData, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 2));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 2, &read_data));
   EXPECT_EQ("89", read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 3));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 3, &read_data));
   EXPECT_EQ("789", read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 5));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 5, &read_data));
   EXPECT_EQ("56789", read_data);
 }
 
-TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileWithZeros) {
+TEST_F(FeedbackUtilTest, ReadEndOfFileWithZeros) {
   const size_t test_size = 10;
   std::string test_data("abcd\0\0\0\0hi", test_size);
   std::string read_data;
@@ -71,27 +73,27 @@ TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileWithZeros) {
   WriteFile(file_path, test_data.data(), test_size);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 15));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 15, &read_data));
   EXPECT_EQ(test_data, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 10));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 10, &read_data));
   EXPECT_EQ(test_data, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 2));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 2, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 2, 2), read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 3));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 3, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 3, 3), read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 5));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 5, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 5, 5), read_data);
 }
 
-TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileMedium) {
+TEST_F(FeedbackUtilTest, ReadEndOfFileMedium) {
   std::string test_data = base::RandBytesAsString(10000);  // 10KB data
   std::string read_data;
 
@@ -102,24 +104,24 @@ TEST_F(DebugDaemonLogSourceTest, ReadEndOfFileMedium) {
   WriteFile(file_path, test_data.data(), test_size);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 15000));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 15000, &read_data));
   EXPECT_EQ(test_data, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 10000));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 10000, &read_data));
   EXPECT_EQ(test_data, read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 1000));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 1000, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 1000, 1000), read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 300));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 300, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 300, 300), read_data);
 
   read_data.clear();
-  EXPECT_TRUE(ReadEndOfFile(file_path, &read_data, 175));
+  EXPECT_TRUE(feedback_util::ReadEndOfFile(file_path, 175, &read_data));
   EXPECT_EQ(test_data.substr(test_size - 175, 175), read_data);
 }
 
-}  // namespace system_logs
+}  // namespace feedback_util
