0c4572c29ed752832057a9b89b4e7c2252013992
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-08-24 19:18:39 +0000

    [CrOS settings] Migrate cellular_setup/provisioning_page to Polymer3
    Change-Id: Ia276e9af51540df765527977105ff2366eecf878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359751
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801101}

--- chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
+++ chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
@@ -44,6 +44,7 @@ GEN('#include "content/public/test/browser_test.h"');
 [
   ['BasePage', 'cellular_setup/base_page_test.js', []],
   ['FinalPage', 'cellular_setup/final_page_test.js', []],
+  ['ProvisioningPage', 'cellular_setup/provisioning_page_test.js',[]],
   ['SimDetectPage', 'cellular_setup/sim_detect_page_test.js', []],
 ].forEach(test => registerTest('CellularSetup', 'cellular-setup', ...test));
 // clang-format on
--- chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_v3_browsertest.js
+++ chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_v3_browsertest.js
@@ -24,6 +24,7 @@ GEN('#include "chromeos/constants/chromeos_features.h"');
 
 [['BasePage', 'cellular_setup/base_page_test.m.js'],
  ['FinalPage', 'cellular_setup/final_page_test.m.js'],
+ ['ProvisioningPage', 'cellular_setup/provisioning_page_test.m.js'],
  ['SimDetectPage', 'cellular_setup/sim_detect_page_test.m.js'],
 ].forEach(test => registerTest('CellularSetup', ...test));
 // clang-format on
@@ -51,6 +52,7 @@ function registerTest(componentName, testName, module, caseName) {
       return {
         enabled: [
           'chromeos::features::kOsSettingsPolymer3',
+          'chromeos::features::kUpdatedCellularActivationUi',
         ],
       };
     }
