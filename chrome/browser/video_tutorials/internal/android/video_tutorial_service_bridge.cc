2a1071111d3a68ef7640634bb1c9989c8b81ee82
Shakti Sahu <shaktisahu@chromium.org>
2020-08-25 08:47:05 +0000

    Video Tutorials : Modularized JNI bridge
    This CL moves the VideoTutorialServiceFactory and the JNI bridge into
    the internal target. A corresponding public factory class is generated
    from the internal factory using android_library_factory function. The
    JNI function lives in video_tutorial_service_bridge_factory.cc which is
    included in chrome/browser target while still living in
    chrome/browser/video_tutorials/internal.
    
    Bug: 1120097
    Change-Id: I4cbc7e2763012e3780a28f58bf14e3374464e20e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368474
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801312}

--- chrome/browser/video_tutorials/internal/android/video_tutorial_service_bridge.cc
+++ chrome/browser/video_tutorials/internal/android/video_tutorial_service_bridge.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/video_tutorials/android/video_tutorial_service_bridge.h"
+#include "chrome/browser/video_tutorials/internal/android/video_tutorial_service_bridge.h"
 
 #include <memory>
 #include <vector>
