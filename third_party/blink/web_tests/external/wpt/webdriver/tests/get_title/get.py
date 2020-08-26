bbce185d908aa81682048824c992ec869883ba4d
Amr Aboelkher <amraboelkher@google.com>
2020-08-26 13:58:48 +0000

    [Sheriff] Revert "Import wpt@a75c689d52f70baf607c8e6c1bb84c57f48badda"
    This reverts commit dc5e5f36bcc0a92b77ead0788c79390c151a4086.
    
    Reason for revert: linux-trusty-rel build and external blink web_driver tests are failing, and that's a possible root cause. See: https://ci.chromium.org/p/chromium/builders/ci/linux-trusty-rel/15138
    
    Original change's description:
    > Import wpt@a75c689d52f70baf607c8e6c1bb84c57f48badda
    > 
    > Using wpt-import in Chromium c21b8329bb14c0c12dbd71315fa40b7dab09e24b.
    > With Chromium commits locally applied on WPT:
    > 9f52c7e47e "COOP: add reporting to redirects"
    > d18db80440 "Fix test flake in animation-state-changes-positive-playback-rate.html"
    > 
    > 
    > Note to sheriffs: This CL imports external tests and adds
    > expectations for those tests; if this CL is large and causes
    > a few new failures, please fix the failures by adding new
    > lines to TestExpectations rather than reverting. See:
    > https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    > 
    > Directory owners for changes in this CL:
    > futhark@chromium.org, andruud@chromium.org, ericwilligers@chromium.org, shend@chromium.org:
    >   external/wpt/css/css-cascade
    > 
    > NOAUTOREVERT=true
    > TBR=robertma@google.com
    > 
    > No-Export: true
    > Change-Id: I1a13539eede33cfc0ecb838dd86dbcd04e84481d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377457
    > Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    > Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#801734}
    
    TBR=robertma@google.com,wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com
    
    Change-Id: I35b79183bad9975df7d6f87fb6bbe2f50cb9e2ed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377493
    Reviewed-by: Amr Aboelkher <amraboelkher@google.com>
    Commit-Queue: Amr Aboelkher <amraboelkher@google.com>
    Cr-Commit-Position: refs/heads/master@{#801769}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/get_title/get.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/get_title/get.py
@@ -1,7 +1,10 @@
-from six import text_type
-
 from tests.support.asserts import assert_error, assert_success
 from tests.support.inline import inline
+from tests.support.sync import Poll
+
+
+def read_global(session, name):
+    return session.execute_script("return %s;" % name)
 
 
 def get_title(session):
@@ -9,44 +12,56 @@ def get_title(session):
         "GET", "session/{session_id}/title".format(**vars(session)))
 
 
-def test_payload(session):
-    session.start()
-
-    response = get_title(session)
-    value = assert_success(response)
-    assert isinstance(value, text_type)
-
-
 def test_no_browsing_context(session, closed_window):
     response = get_title(session)
     assert_error(response, "no such window")
 
 
-def test_with_duplicated_title(session):
+def test_title_from_top_context(session):
+    session.url = inline("<title>Foobar</title><h2>Hello</h2>")
+
+    result = get_title(session)
+    assert_success(result, read_global(session, "document.title"))
+
+
+def test_title_with_duplicate_element(session):
     session.url = inline("<title>First</title><title>Second</title>")
 
     result = get_title(session)
-    assert_success(result, "First")
+    assert_success(result, read_global(session, "document.title"))
 
 
-def test_without_title(session):
+def test_title_without_element(session):
     session.url = inline("<h2>Hello</h2>")
 
     result = get_title(session)
-    assert_success(result, "")
+    assert_success(result, read_global(session, "document.title"))
 
 
-def test_after_modification(session):
+def test_title_after_modification(session):
+    def title():
+        return read_global(session, "document.title")
+
     session.url = inline("<title>Initial</title><h2>Hello</h2>")
     session.execute_script("document.title = 'Updated'")
 
-    result = get_title(session)
-    assert_success(result, "Updated")
+    wait = Poll(session, message='Document title does not match "{}"'.format(title()))
+    wait.until(lambda s: assert_success(get_title(s)) == title())
 
 
-def test_strip_and_collapse(session):
+def test_title_strip_and_collapse(session):
     document = "<title>   a b\tc\nd\t \n e\t\n </title><h2>Hello</h2>"
     session.url = inline(document)
 
     result = get_title(session)
-    assert_success(result, "a b c d e")
+    assert_success(result, read_global(session, "document.title"))
+
+
+def test_title_from_frame(session, create_frame):
+    session.url = inline("<title>Parent</title>parent")
+
+    session.switch_frame(create_frame())
+    session.switch_frame(create_frame())
+
+    result = get_title(session)
+    assert_success(result, "Parent")
