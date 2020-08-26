fb07e7f9d4eef1c699c4e99418bdba5a651efb81
Luciano Pacheco <lucmult@chromium.org>
2020-08-26 08:55:51 +0000

    Video Player: Remove non-native controls
    Remove JS code, CSS style and images used by non-native controls.
    
    Native controls have been enabled by default for a few milestones (since
    M73) crbug.com/909963#c7
    
    This CL dead code since the feature flag has been removed: CL:2018089.
    
    Tested manually opening one video as well multiple videos and switching
    between them.
    
    Bug: 909963
    Change-Id: I3478fb03cb33acff2d58e527af48a31879c37c7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362171
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801695}

--- ui/file_manager/video_player/js/video_player_scripts.js
+++ ui/file_manager/video_player/js/video_player_scripts.js
@@ -27,12 +27,6 @@
 // <include src="../../../webui/resources/js/cr/event_target.js">
 
 // <include src="../../../webui/resources/js/cr/ui/array_data_model.js">
-// <include src="../../../webui/resources/js/cr/ui/position_util.js">
-// <include src="../../../webui/resources/js/cr/ui/menu_item.js">
-// <include src="../../../webui/resources/js/cr/ui/menu.js">
-// <include src="../../../webui/resources/js/cr/ui/menu_button.js">
-// <include src="../../../webui/resources/js/cr/ui/context_menu_handler.js">
-
 (function() {
 'use strict';
 
@@ -46,17 +40,8 @@
 /* TODO(tapted): Remove the util.js dependency */
 // <include src="../../file_manager/common/js/util.js">
 
-// <include src="../../base/js/mediasession_types.js">
 // <include src="../../base/js/volume_manager_types.js">
 // <include src="../../base/js/filtered_volume_manager.js">
-
-// <include src="cast/cast_extension_discoverer.js">
-// <include src="cast/cast_video_element.js">
-// <include src="cast/media_manager.js">
-// <include src="cast/caster.js">
-
-// <include src="media_controls.js">
-// <include src="mouse_inactivity_watcher.js">
 
 // <include src="video_player_native_controls.js">
 // <include src="video_player.js">
